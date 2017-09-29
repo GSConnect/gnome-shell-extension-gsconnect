2f220c83715b94986e17215ce372c0485da79ef9
Andy Holmes <andrew.g.r.holmes@gmail.com>
2017-09-29 02:25:24 -0700

    restructure

--- src/service/plugins/telephony.js
+++ src/service/plugins/telephony.js
@@ -76,7 +76,9 @@ var Plugin = new Lang.Class({
         Gtk.IconTheme.get_default().add_resource_path("/icons");
     },
     
-    // TODO
+    // TODO: try and block incoming sms "notifications"
+    //       music pause, etc
+    //       
     handlePacket: function (packet) {
         // There are six possible variables:
         //    * "event"             missedCall, ringing, sms or talking
@@ -86,7 +88,7 @@ var Plugin = new Lang.Class({
         //    * "phoneThumbnail"    base64 ByteArray/Pixmap (may be empty)
         //    * "isCancel"          If true the packet should be ignored
         
-        let sender, note;
+        let sender;
         
          // FIXME: not sure what to do here...
 //        if (!packet.body.phoneNumber.length) {
@@ -107,126 +109,133 @@ var Plugin = new Lang.Class({
         if (packet.body.hasOwnProperty("isCancel") && packet.body.isCancel) {
             return;
         } else if (packet.body.event === "missedCall") {
-            this._dbus.emit_signal("missedCall",
-                new GLib.Variant(
-                    "(ss)",
-                    [packet.body.phoneNumber,
-                    packet.body.contactName]
-                )
-            );
-            
-            if (this.settings.notify_missedCall) {
-                note = new Notify.Notification({
-                    app_name: "GSConnect",
-                    id: packet.id / 1000,
-                    summary: _("%s - Missed Call").format(this.device.name),
-                    body: _("Missed call from %s").format(sender),
-                    icon_name: "call-missed-symbolic"
-                });
-                
-                note.show();
-            }
-            
-        // TODO: music pause, etc
+            this.handleMissedCall(sender, packet);
         } else if (packet.body.event === "ringing") {
-            this._dbus.emit_signal("ringing",
-                new GLib.Variant(
-                    "(ss)",
-                    [packet.body.phoneNumber,
-                    packet.body.contactName]
-                )
-            );
-            
-            if (this.settings.notify_ringing) {
-                let note = new Notify.Notification({
-                    app_name: "GSConnect",
-                    id: packet.id / 1000,
-                    summary: _("%s Ringing").format(this.device.name),
-                    body: _("Incoming call from %s").format(sender),
-                    icon_name: "call-start-symbolic"
-                });
-                
-                note.add_action(
-                    "notify_sms",
-                    _("Mute"),
-                    Lang.bind(this, this.mute)
-                );
-                
-                note.show();
-            }
-        // TODO: not really complete
+            this.handleRinging(sender, packet);
         } else if (packet.body.event === "sms") {
-            this._dbus.emit_signal("sms",
-                new GLib.Variant(
-                    "(ssss)",
-                    [packet.body.phoneNumber,
-                    packet.body.contactName,
-                    packet.body.messageBody,
-                    packet.body.phoneThumbnail] // FIXME: bytearray.pixmap
-                )
-            );
-            
-            this.emit(
-                "sms",
-                packet.body.phoneNumber,
-                packet.body.contactName,
-                packet.body.messageBody,
-                packet.body.phoneThumbnail
-            );
-            
-            // FIXME: urgency
-            //        block matching notification somehow?
-            if (this.settings.autoreply_sms) {
-                this.reply(null, "autoreply_sms", packet.body);
-            } else if (this.settings.notify_sms) {
-                let sender;
-                
-                if (packet.body.contactName !== "") {
-                    sender = packet.body.contactName;
-                } else {
-                    sender = packet.body.phoneNumber;
-                }
-            
-                let note = new Notify.Notification({
-                    app_name: "GSConnect",
-                    id: packet.id / 1000,
-                    summary: sender,
-                    body: packet.body.messageBody,
-                    icon_name: "phone-symbolic"
-                });
-                
-                note.add_action(
-                    "notify_sms", // action char
-                    _("Reply"), // label
-                    Lang.bind(this, this.reply, packet.body)
-                );
-                
-                note.show();
-            }
-        
-        // TODO: music pause, etc
+            this.handleSMS(sender, packet);
         } else if (packet.body.event === "talking") {
-            this._dbus.emit_signal("talking",
-                new GLib.Variant(
-                    "(ss)",
-                    [packet.body.phoneNumber,
-                    packet.body.contactName]
-                )
-            );
-            
-            if (this.settings.notify_talking) {
-                note = new Notify.Notification({
-                    app_name: "GSConnect",
-                    id: packet.id / 1000,
-                    summary: _("%s - Talking").format(this.device.name),
-                    body: _("Call in progress with %s").format(sender),
-                    icon_name: "call-start-symbolic"
-                });
-                
-                note.show();
-            }
+            this.handleTalking(sender, packet);
         } else {
             log("Unknown telephony event: " + packet.body.event);
+        }
+    },
+    
+    handleMissedCall: function (sender, packet) {
+        this._dbus.emit_signal("missedCall",
+            new GLib.Variant(
+                "(ss)",
+                [packet.body.phoneNumber,
+                packet.body.contactName]
+            )
+        );
+        
+        if (this.settings.notify_missedCall) {
+            let note = new Notify.Notification({
+                app_name: "GSConnect",
+                id: Number(packet.id.toString().slice(2)),
+                summary: _("%s - Missed Call").format(this.device.name),
+                body: _("Missed call from %s").format(sender),
+                icon_name: "call-missed-symbolic"
+            });
+            
+            note.show();
+        }
+    },
+    
+    handleRinging: function (sender, packet) {
+        // TODO: music pause, etc
+        this._dbus.emit_signal("ringing",
+            new GLib.Variant(
+                "(ss)",
+                [packet.body.phoneNumber,
+                packet.body.contactName]
+            )
+        );
+        
+        if (this.settings.notify_ringing) {
+            let note = new Notify.Notification({
+                app_name: "GSConnect",
+                id: Number(packet.id.toString().slice(2)),
+                summary: _("%s Ringing").format(this.device.name),
+                body: _("Incoming call from %s").format(sender),
+                icon_name: "call-start-symbolic"
+            });
+            
+            note.add_action(
+                "notify_sms",
+                _("Mute"),
+                Lang.bind(this, this.mute)
+            );
+            
+            note.show();
+        }
+    },
+    
+    handleSMS: function (sender, packet) {
+        // TODO: not really complete
+        this._dbus.emit_signal("sms",
+            new GLib.Variant(
+                "(ssss)",
+                [packet.body.phoneNumber,
+                packet.body.contactName,
+                packet.body.messageBody,
+                packet.body.phoneThumbnail] // FIXME: bytearray.pixmap
+            )
+        );
+        
+        this.emit(
+            "sms",
+            packet.body.phoneNumber,
+            packet.body.contactName,
+            packet.body.messageBody,
+            packet.body.phoneThumbnail
+        );
+        
+        // FIXME: urgency
+        //        block matching notification somehow?
+        if (this.settings.autoreply_sms) {
+            this.reply(null, "autoreply_sms", packet.body);
+        } else if (this.settings.notify_sms) {
+        
+            let note = new Notify.Notification({
+                app_name: "GSConnect",
+                id: Number(packet.id.toString().slice(2)),
+                summary: sender,
+                body: packet.body.messageBody,
+                icon_name: "phone-symbolic"
+            });
+            
+            note.add_action(
+                "notify_sms", // action char
+                _("Reply"), // label
+                Lang.bind(this, this.reply, packet.body)
+            );
+            
+            note.show();
+        }
+    },
+    
+    handleTalking: function (sender, packet) {
+        // TODO: music pause, etc
+        this._dbus.emit_signal("talking",
+            new GLib.Variant(
+                "(ss)",
+                [packet.body.phoneNumber,
+                packet.body.contactName]
+            )
+        );
+        
+        if (this.settings.notify_talking) {
+            note = new Notify.Notification({
+                app_name: "GSConnect",
+                id: Number(packet.id.toString().slice(2)),
+                summary: _("%s - Talking").format(this.device.name),
+                body: _("Call in progress with %s").format(sender),
+                icon_name: "call-start-symbolic"
+            });
+            
+            note.show();
         }
     },
     
@@ -241,11 +250,9 @@ var Plugin = new Lang.Class({
     },
     
     reply: function (notification, action, args) {
-        log("TelephonyPlugin._open_sms()");
+        log("TelephonyPlugin.reply()");
         
-        log("REPLY ARGS: " + JSON.stringify(args));
-        
-        //
+        // Get the current open windows
         let windows = this.device.daemon.get_windows();
         let window = false;
         
