6d5914399126f70db50361e226f8e5c740577573
Andy Holmes <andrew.g.r.holmes@gmail.com>
2017-10-01 14:11:44 -0700

    fix keybindings widget

--- src/widgets/keybindings.js
+++ src/widgets/keybindings.js
@@ -14,21 +14,28 @@ const Gtk = imports.gi.Gtk;
  * A Custom Gtk.TreeView for displaying and modifying keybinding profiles
  */
 var TreeView = new Lang.Class({
-    Name: "KeybindingView",
+    Name: "GSConnectKeybindingsTreeView",
     Extends: Gtk.TreeView,
     
     _init: function () {
         this.parent({
-            enable_grid_lines: true,
             headers_visible: false,
-            hexpand: true,
-            margin_top: 6
+            hexpand: true
+        });
+        
+        this._callback = undefined;
+        
+        this.shellBus = new Gio.DBusProxy({
+            gConnection: Gio.DBus.session,
+            gName: "org.gnome.Shell",
+            gObjectPath: "/org/gnome/Shell",
+            gInterfaceName: "org.gnome.Shell"
         });
         
         let listStore = new Gtk.ListStore();
         listStore.set_column_types([
-            GObject.TYPE_INT,       // Index
-            GObject.TYPE_STRING,    // Action
+            GObject.TYPE_STRING,    // ID
+            GObject.TYPE_STRING,    // Description
             GObject.TYPE_INT,       // Key
             GObject.TYPE_INT        // Modifiers
         ]);
@@ -55,143 +62,114 @@ var TreeView = new Lang.Class({
         accelCol.add_attribute(this.accelCell, "accel-key", 2);
         accelCol.add_attribute(this.accelCell, "accel-mods", 3);
         this.append_column(accelCol);
+        
+        //
+        this.accelCell.connect("accel-edited", (renderer, path, key, mods) => {
+            let [success, iter] = this.model.get_iter_from_string(path);
+            
+            if (success && mods > 0) {
+                let id = this.model.get_value(iter, 0);
+                let binding = Gtk.accelerator_name(key, mods);
+                
+                // Check for existing instance of binding
+                if (this._check(binding)) {
+                    let accels = this.getAccels();
+                    accels[id] = binding;
+                    this.setAccels(accels);
+                    
+                    if (typeof this._callback === "function") {
+                        this._callback(this.getAccels());
+                    }
+                }
+            }
+        });
+
+        this.accelCell.connect("accel-cleared", (renderer, path) => {
+            let [success, iter] = this.model.get_iter_from_string(path);
+            
+            if (success) {
+                let index = this.model.get_value(iter, 0);
+                this.model.set(iter, [2, 3], [0, 0]);
+                
+                if (typeof this._callback === "function") {
+                    this._callback(this.getAccels());
+                }
+            }
+        });
+    },
+    
+    /**
+     * Add an accelerator to configure
+     *
+     * @param {string} id - a text id
+     * @param {string} description - A description of the accelerator's purpose
+     * @param {number} key - keyval
+     * @param {number} mods - mod mask
+     */
+    addAccel: function (id, description, key, mods) {
+        this.model.set(
+            this.model.append(),
+            [0, 1, 2, 3],
+            [id, description, key, mods]
+        );
+    },
+    
+    getAccels: function () {
+        let profile = {};
+        
+        this.model.foreach((model, path, iter, user_data) => {
+            let id = model.get_value(iter, 0);
+            let key = model.get_value(iter, 2);
+            let mods = model.get_value(iter, 3);
+            
+            if (key === 0 || mods === 0) {
+                profile[id] = "";
+            } else {
+                profile[id] = Gtk.accelerator_name(key, mods);
+                log("ACCEL: " + Gtk.accelerator_name(key, mods));
+            }
+        });
+        
+        return profile;
     },
     
     /**
      * Load a profile of keybindings
      *
-     * @param {Array} profile - The section widget to attach to
+     * @param {object} profile - An object with the structure:
+     *
+     *     { <accel-id>: <accel-string>, ... }
+     *
      */
-    load_profile: function (profile) {
+    setAccels: function (profile) {
         if (profile === undefined) {
             this.model.foreach((model, path, iter, user_data) => {
                 model.set(iter, [2, 3], [0, 0]);
             });
         } else {
             this.model.foreach((model, path, iter, user_data) => {
-                let index = model.get_value(iter, 0);
-                model.set(iter, [2, 3], Gtk.accelerator_parse(profile[index]));
+                let id = model.get_value(iter, 0);
+                
+                if (profile.hasOwnProperty(id)) {
+                    model.set(
+                        iter,
+                        [2, 3],
+                        Gtk.accelerator_parse(profile[id])
+                    );
+                }
             });
         }
     },
     
-    /**
-     *
-     */
-    add_accel: function (index, description, key, mods) {
-        this.model.set(
-            this.model.append(),
-            [0, 1, 2, 3],
-            [index, description, key, mods]
-        );
-    }
-});
-
-
-/** 
- * A composite widget that combines KeybindingProfileBox and KeybindingView
- * into a single settings panel for managing device keybinding profiles.
- */
-var KeybindingWidget = new Lang.Class({
-    Name: "KeybindingWidget",
-    Extends: Gtk.ListBoxRow,
-    
-    _init: function () {
-        this.parent({
-            visible: true,
-            can_focus: true,
-            activatable: false,
-            selectable: false
-        });
-        
-        this.shellBus = new Gio.DBusProxy({
-            gConnection: Gio.DBus.session,
-            gName: "org.gnome.Shell",
-            gObjectPath: "/org/gnome/Shell",
-            gInterfaceName: "org.gnome.Shell"
-        });
-        
-        this.grid = new Gtk.Grid({
-            visible: true,
-            can_focus: false,
-            column_spacing: 16,
-            row_spacing: 6,
-            margin_left: 12,
-            margin_top: 6,
-            margin_bottom: 6,
-            margin_right: 12
-        });
-        this.add(this.grid);
-        
-        // Extension Keybindings
-        let extSchema = Schema.get_key("extension-keybindings");
-        let extSummary = new Gtk.Label({
-            visible: true,
-            can_focus: false,
-            xalign: 0,
-            hexpand: true,
-            label: extSchema.get_summary()
-        });
-        this.grid.attach(extSummary, 0, 0, 1, 1);
-        
-        let extDesc = new Gtk.Label({
-            visible: true,
-            can_focus: false,
-            xalign: 0,
-            hexpand: true,
-            label: extSchema.get_description(),
-            wrap: true
-        });
-        extDesc.get_style_context().add_class("dim-label");
-        this.grid.attach(extDesc, 0, 1, 1, 1);
-        
-        this.extView = new KeybindingView();
-        this.extView.margin_bottom = 12;
-        this.extView.add_accel(0, _("Open extension menu"), 0, 0);
-        this.extView.add_accel(1, _("Discover Devices"), 0, 0);
-        this.extView.add_accel(2, _("Open extension preferences"), 0, 0);
-        this.extView.add_accel(3, _("Open service settings"), 0, 0);
-        this.grid.attach(this.extView, 0, 2, 2, 1);
-        
-        this.extView.accelCell.connect("accel-edited", (renderer, path, key, mods) => {
-            let [success, iter] = this.extView.model.get_iter_from_string(path);
-            
-            if (success && mods > 0) {
-                let index = this.extView.model.get_value(iter, 0);
-                let binding = Gtk.accelerator_name(key, mods);
-                
-                // Check for existing instance of binding
-                if (this._check(binding)) {
-                    this._extKeys[index] = binding;
-                    Settings.set_strv("extension-keybindings", this._extKeys);
-                    this.extView.load_profile(this._extKeys);
-                }
-            }
-        });
-
-        this.extView.accelCell.connect("accel-cleared", (renderer, path) => {
-            let [success, iter] = this.extView.model.get_iter_from_string(path);
-            
-            if (success) {
-                let index = this.extView.model.get_value(iter, 0);
-                this.extView.model.set(iter, [2, 3], [0, 0]);
-                this._extKeys[index] = "";
-                Settings.set_strv("extension-keybindings", this._extKeys);
-            }
-        });
-        
-        this._extKeys = Settings.get_strv("extension-keybindings");
-        this.extView.load_profile(this._extKeys);
+    setCallback: function (callback) {
+        if (typeof callback === "function") {
+            this._callback = callback;
+        } else {
+            throw Error("arg 'callback' must be a function");
+        }
     },
     
     _check: function (binding) {
-        if (this._extKeys.indexOf(binding) > -1) {
-            this._extKeys[this._extKeys.indexOf(binding)] = "";
-            Settings.set_strv("extension-keybindings", this._extKeys);
-            this.extView.load_profile(this._extKeys);
-            return true;
-        }
-        
         // Check someone else isn't already using the binding
         let action = this.shellBus.call_sync(
             "GrabAccelerator",
@@ -225,6 +203,5 @@ var KeybindingWidget = new Lang.Class({
         }
     }
 });
-
 
 
