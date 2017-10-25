5c7576071c6be5636c24a597bfe34ff6eb0dd356
Andy Holmes <andrew.g.r.holmes@gmail.com>
2017-10-25 14:49:05 -0700

    Telephony Widgets: remove levenshtein sort

--- src/widgets/telephony.js
+++ src/widgets/telephony.js
@@ -136,7 +136,7 @@ var ContactCompletion = new Lang.Class({
             GdkPixbuf.Pixbuf        // Type Icon
         ]);
         listStore.set_sort_column_id(0, Gtk.SortType.ASCENDING);
-        listStore.set_sort_func(1, this._levenshtein, null, null);
+        listStore.set_sort_func(0, this._sort);
         this.set_model(listStore);
         
         // Title
@@ -333,29 +333,8 @@ var ContactCompletion = new Lang.Class({
         return true;
     },
     
-    /** 
-     * A levenshtein sort function
-     * See: https://gist.github.com/andrei-m/982927#gistcomment-2059365
-     */
-    _levenshtein: function (model, a, b, user_data) {
-	    var tmp;
-	    if (a.length === 0) { return b.length; }
-	    if (b.length === 0) { return a.length; }
-	    if (a.length > b.length) { tmp = a; a = b; b = tmp; }
-
-	    var i, j, res, alen = a.length, blen = b.length, row = Array(alen);
-	    for (i = 0; i <= alen; i++) { row[i] = i; }
-
-	    for (i = 1; i <= blen; i++) {
-		    res = i;
-		    for (j = 1; j <= alen; j++) {
-			    tmp = row[j - 1];
-			    row[j - 1] = res;
-			    res = b[i - 1] === a[j - 1] ? tmp : Math.min(tmp + 1, Math.min(res + 1, row[j] + 1));
-		    }
-	    }
-	    
-	    return res;
+    _sort: function (model, a, b, user_data) {
+        return model.get_value(a, 0).localeCompare(model.get_value(b, 0));
     }
 });
 
@@ -1017,10 +996,10 @@ var ConversationWindow = new Lang.Class({
         let avatar;
         
         try {
-            //avatar = new ContactAvatar(
-            //    phoneThumbnail,
-            //    32
-            //);
+//            avatar = new ContactAvatar(
+//                recipient.phoneThumbnail,
+//                32
+//            );
             
             let bytes = GLib.base64_decode(recipient.phoneThumbnail);
             avatar = new Gtk.Image({
