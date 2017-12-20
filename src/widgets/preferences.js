fe7adec99d03a47ede008adc98e895a0dc50e9af
Andy Holmes <andrew.g.r.holmes@gmail.com>
2017-12-20 07:29:07 -0800

    cleanup imports

--- src/widgets/preferences.js
+++ src/widgets/preferences.js
@@ -1,8 +1,9 @@
 "use strict";
 
+const Lang = imports.lang;
+
 const Gio = imports.gi.Gio;
 const Gtk = imports.gi.Gtk;
-const Lang = imports.lang;
 
 // Local Imports
 imports.searchPath.push(ext.datadir);
