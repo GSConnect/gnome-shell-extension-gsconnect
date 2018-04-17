1c2db0d6973a5d7358f8a38ca514e9fe4a6d73b8
Andy Holmes <andrew.g.r.holmes@gmail.com>
2018-04-17 14:29:59 -0700

    FindMyPhone: misc cleanup, use single-quotes

--- src/service/plugins/findmyphone.js
+++ src/service/plugins/findmyphone.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 const Gdk = imports.gi.Gdk;
 const Gio = imports.gi.Gio;
@@ -13,29 +13,29 @@ const PluginsBase = imports.service.plugins.base;
 
 
 var Metadata = {
-    id: "org.gnome.Shell.Extensions.GSConnect.Plugin.FindMyPhone",
-    incomingCapabilities: ["kdeconnect.findmyphone.request"],
-    outgoingCapabilities: ["kdeconnect.findmyphone.request"],
+    id: 'org.gnome.Shell.Extensions.GSConnect.Plugin.FindMyPhone',
+    incomingCapabilities: ['kdeconnect.findmyphone.request'],
+    outgoingCapabilities: ['kdeconnect.findmyphone.request'],
     actions: {
         find: {
-            summary: _("Locate"),
-            description: _("Find a device by making it ring"),
-            icon_name: "find-location-symbolic",
+            summary: _('Locate'),
+            description: _('Find a device by making it ring'),
+            icon_name: 'find-location-symbolic',
 
             signature: null,
             incoming: [],
-            outgoing: ["kdeconnect.findmyphone.request"],
+            outgoing: ['kdeconnect.findmyphone.request'],
             allow: 2
         }
     },
     events: {
         find: {
-            summary: _("Locate"),
-            description: _("Find a device by making it ring"),
+            summary: _('Locate'),
+            description: _('Find a device by making it ring'),
 
             signature: null,
             incoming: [],
-            outgoing: ["kdeconnect.findmyphone.request"],
+            outgoing: ['kdeconnect.findmyphone.request'],
             allow: 4
         }
     }
@@ -47,22 +47,24 @@ var Metadata = {
  * https://github.com/KDE/kdeconnect-kde/tree/master/plugins/findmyphone
  */
 var Plugin = GObject.registerClass({
-    GTypeName: "GSConnectFindMyPhonePlugin",
+    GTypeName: 'GSConnectFindMyPhonePlugin',
 }, class Plugin extends PluginsBase.Plugin {
 
     _init(device) {
-        super._init(device, "findmyphone");
+        super._init(device, 'findmyphone');
 
         this._desktop = new Gio.Settings({
-            schema_id: "org.gnome.system.location"
+            schema_id: 'org.gnome.system.location'
         });
 
         this._cancellable = null;
         this._dialog = null;
+
+        this.device.menu.add_action('find', Metadata.actions.find);
     }
 
     handlePacket(packet) {
-        debug("FindMyPhone: handlePacket()");
+        debug('FindMyPhone: handlePacket()');
 
         if (this.allow & 4) {
             this._handleFind();
@@ -73,18 +75,18 @@ var Plugin = GObject.registerClass({
      * Local Methods
      */
     _handleFind() {
-        debug("FindMyPhone: _ring()");
+        debug('FindMyPhone: _ring()');
 
         if (this._cancellable || this._dialog) {
             this._endFind();
         }
 
         this._cancellable = new Gio.Cancellable();
-        Sound.loopThemeSound("phone-incoming-call", this._cancellable);
+        Sound.loopThemeSound('phone-incoming-call', this._cancellable);
 
         this._dialog = new Gtk.MessageDialog({
-            text: _("Locate Device"),
-            secondary_text: _("%s asked to locate this device").format(this.device.name),
+            text: _('Locate Device'),
+            secondary_text: _('%s asked to locate this device').format(this.device.name),
             urgency_hint: true,
             window_position: Gtk.WindowPosition.CENTER_ALWAYS,
             application: Gio.Application.get_default(),
@@ -92,13 +94,13 @@ var Plugin = GObject.registerClass({
             skip_taskbar_hint: true,
             visible: true
         });
-        this._dialog.connect("delete-event", () => this._endFind());
-        this._dialog.connect("key-press-event", (dialog, event) => {
+        this._dialog.connect('delete-event', () => this._endFind());
+        this._dialog.connect('key-press-event', (dialog, event) => {
             if (event.get_keyval()[1] === Gdk.KEY_Escape) {
                 this._endFind();
             }
         });
-        this._dialog.add_button(_("Found"), -4).connect("clicked", () => {
+        this._dialog.add_button(_('Found'), -4).connect('clicked', () => {
             this._endFind();
         });
         this._dialog.set_keep_above(true);
@@ -122,12 +124,12 @@ var Plugin = GObject.registerClass({
 
         // FIXME
         if (!(this.allow & 2)) {
-            debug(new Error("Operation not permitted: " + packet.type));
+            debug(new Error('Operation not permitted: ' + packet.type));
         }
 
         this.device.sendPacket({
             id: 0,
-            type: "kdeconnect.findmyphone.request",
+            type: 'kdeconnect.findmyphone.request',
             body: {}
         });
     }
