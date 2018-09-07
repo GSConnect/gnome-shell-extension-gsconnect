5f415fb3a1205d2aaf0dca5d2d94a7d0e3336bae
Andy Holmes <andrew.g.r.holmes@gmail.com>
2018-09-07 09:31:23 -0700

    __init__: add 'certificate_der' property to Gio.TlsCertificate

--- src/service/__init__.js
+++ src/service/__init__.js
@@ -219,6 +219,29 @@ Object.defineProperties(Gio.TlsCertificate.prototype, {
             return this.__common_name;
         },
         enumerable: true
+    },
+
+    /**
+     * The common name of the certificate.
+     */
+    'certificate_der': {
+        get: function() {
+            if (!this.__certificate_der) {
+                let proc = new Gio.Subprocess({
+                    argv: ['openssl', 'x509', '-outform', 'der', '-inform', 'pem'],
+                    flags: Gio.SubprocessFlags.STDIN_PIPE | Gio.SubprocessFlags.STDOUT_PIPE
+                });
+                proc.init(null);
+
+                let stdout = proc.communicate(new GLib.Bytes(this.certificate_pem), null)[1];
+                this.__certificate_der = stdout.toArray();
+
+                proc.wait_check(null);
+            }
+
+            return this.__certificate_der;
+        },
+        enumerable: true
     }
 });
 
