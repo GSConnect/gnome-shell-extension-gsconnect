119b240d46be76a3f1257d91b017c64fc36a6214
Andy Holmes <andrew.g.r.holmes@gmail.com>
2018-09-30 21:44:46 -0700

    Contacts: add first-draft regex for contact number matching

--- src/service/components/contacts.js
+++ src/service/components/contacts.js
@@ -6,6 +6,10 @@ const GLib = imports.gi.GLib;
 const GObject = imports.gi.GObject;
 
 
+// A simple regex for separating the international prefix from the local number
+var _numberRegex = /\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\W*(\d\W*\d\W*\d\W*\d\W*\d\W*\d\W*\d\W*\d\W*(\d{1,2}))$/;
+
+
 //
 
 
