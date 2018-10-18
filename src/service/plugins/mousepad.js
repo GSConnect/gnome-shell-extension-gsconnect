e366d8617ea1815286d0a9044f1521591a2f424e
Andy Holmes <andrew.g.r.holmes@gmail.com>
2018-10-18 07:56:20 -0700

    Mousepad: initial commit of keyboard input handling

--- src/service/plugins/mousepad.js
+++ src/service/plugins/mousepad.js
@@ -14,6 +14,7 @@ var Metadata = {
     label: _('Mousepad'),
     id: 'org.gnome.Shell.Extensions.GSConnect.Plugin.Mousepad',
     incomingCapabilities: [
+        'kdeconnect.mousepad.echo',
         'kdeconnect.mousepad.request',
         'kdeconnect.mousepad.keyboardstate'
     ],
@@ -23,6 +24,14 @@ var Metadata = {
         'kdeconnect.mousepad.keyboardstate'
     ],
     actions: {
+        keyboard: {
+            label: _('Keyboard'),
+            icon_name: 'input-keyboard-symbolic',
+
+            parameter_type: null,
+            incoming: ['kdeconnect.mousepad.echo', 'kdeconnect.mousepad.keyboardstate'],
+            outgoing: ['kdeconnect.mousepad.request']
+        }
     }
 };
 
@@ -171,6 +180,11 @@ var Plugin = GObject.registerClass({
                     this._handleInput(packet.body);
                 }
                 break;
+
+            case 'kdeconnect.mousepad.echo':
+                this._handleEcho(packet.body);
+                break;
+
             case 'kdeconnect.mousepad.keyboardstate':
                 this._handleState(packet);
                 break;
@@ -357,6 +371,22 @@ var Plugin = GObject.registerClass({
         }
     }
 
+    _handleEcho(input) {
+        if (!this._dialog || !this._dialog.visible) {
+            return;
+        }
+
+        if (input.alt || input.ctrl || input.super) {
+            return;
+        }
+
+        if (input.key) {
+            this._dialog.text.buffer.text += input.key;
+        } else if (KeyMap.get(input.specialKey) === Gdk.KEY_BackSpace) {
+            this._dialog.text.emit('backspace');
+        }
+    }
+
     /**
      * Send an echo/ACK of @input, if requested
      *
@@ -395,6 +425,20 @@ var Plugin = GObject.registerClass({
                 state: this.share_control
             }
         });
+    }
+
+    /**
+     * Open the Keyboard Input dialog
+     */
+    keyboard() {
+        if (!this._dialog) {
+            this._dialog = new KeyboardInputDialog({
+                device: this.device,
+                plugin: this
+            });
+        }
+
+        this._dialog.present();
     }
 });
 
@@ -460,4 +504,266 @@ const isAlt = (key) => [Gdk.KEY_Alt_L, Gdk.KEY_Alt_R].includes(key);
 const isCtrl = (key) => [Gdk.KEY_Control_L, Gdk.KEY_Control_R].includes(key);
 const isShift = (key) => [Gdk.KEY_Shift_L, Gdk.KEY_Shift_R].includes(key);
 const isSuper = (key) => [Gdk.KEY_Super_L, Gdk.KEY_Super_R].includes(key);
+
+
+var KeyboardInputDialog = GObject.registerClass({
+    GTypeName: 'GSConnectMousepadKeyboardInputDialog',
+    Properties: {
+        'device': GObject.ParamSpec.object(
+            'device',
+            'Device',
+            'The device associated with this window',
+            GObject.ParamFlags.READWRITE | GObject.ParamFlags.CONSTRUCT_ONLY,
+            GObject.Object
+        ),
+        'plugin': GObject.ParamSpec.object(
+            'plugin',
+            'Plugin',
+            'The mousepad plugin associated with this window',
+            GObject.ParamFlags.READWRITE | GObject.ParamFlags.CONSTRUCT_ONLY,
+            GObject.Object
+        )
+    }
+}, class KeyboardInputDialog extends Gtk.Dialog {
+
+    _init(params) {
+        super._init(Object.assign({
+            use_header_bar: true,
+            default_width: 480,
+            window_position: Gtk.WindowPosition.CENTER
+        }, params));
+
+        let headerbar = this.get_titlebar();
+        headerbar.title = _('Keyboard');
+        headerbar.subtitle = this.device.name;
+
+        // Main Box
+        let content = this.get_content_area();
+        content.border_width = 0;
+
+        // Infobar
+        this.infobar = new Gtk.Revealer();
+        content.add(this.infobar);
+
+        let bar = new Gtk.InfoBar({ message_type: Gtk.MessageType.WARNING });
+        this.infobar.add(bar);
+
+        let infoicon = new Gtk.Image({ icon_name: 'dialog-warning-symbolic' });
+        bar.get_content_area().add(infoicon);
+
+        let infolabel = new Gtk.Label({ label: _('Keyboard not ready') });
+        bar.get_content_area().add(infolabel);
+
+        // Content
+        let layout = new Gtk.Grid({
+            column_spacing: 6,
+            margin: 6
+        });
+        content.add(layout);
+
+        // Modifier Buttons
+        this.shift_label = new Gtk.ShortcutLabel({
+            accelerator: Gtk.accelerator_name(0, Gdk.ModifierType.SHIFT_MASK),
+            halign: Gtk.Align.END,
+            valign: Gtk.Align.START,
+            sensitive: false
+        });
+        layout.attach(this.shift_label, 0, 0, 1, 1);
+
+        this.ctrl_label = new Gtk.ShortcutLabel({
+            accelerator: Gtk.accelerator_name(0, Gdk.ModifierType.CONTROL_MASK),
+            halign: Gtk.Align.END,
+            valign: Gtk.Align.START,
+            sensitive: false
+        });
+        layout.attach(this.ctrl_label, 0, 1, 1, 1);
+
+        this.alt_label = new Gtk.ShortcutLabel({
+            accelerator: Gtk.accelerator_name(0, Gdk.ModifierType.MOD1_MASK),
+            halign: Gtk.Align.END,
+            valign: Gtk.Align.START,
+            sensitive: false
+        });
+        layout.attach(this.alt_label, 0, 2, 1, 1);
+
+        this.super_label = new Gtk.ShortcutLabel({
+            accelerator: Gtk.accelerator_name(0, Gdk.ModifierType.SUPER_MASK),
+            halign: Gtk.Align.END,
+            valign: Gtk.Align.START,
+            sensitive: false
+        });
+        layout.attach(this.super_label, 0, 3, 1, 1);
+
+        // Text Input
+        let scroll = new Gtk.ScrolledWindow({
+            hscrollbar_policy: Gtk.PolicyType.NEVER,
+            shadow_type: Gtk.ShadowType.IN
+        });
+        layout.attach(scroll, 1, 0, 1, 4);
+
+        this.text = new Gtk.TextView({
+            border_width: 6,
+            hexpand: true,
+            vexpand: true,
+            visible: true
+        });
+        scroll.add(this.text);
+
+        this.infobar.connect('notify::reveal-child', this._onState.bind(this));
+        this.plugin.bind_property('state', this.infobar, 'reveal-child', 6);
+
+        this.show_all();
+    }
+
+    vfunc_delete_event(event) {
+        this._ungrab();
+        return this.hide_on_delete();
+    }
+
+    vfunc_key_release_event(event) {
+        if (!this.plugin.state) {
+            return true;
+        }
+
+        let keyvalLower = Gdk.keyval_to_lower(event.keyval);
+        let realMask = event.state & Gtk.accelerator_get_default_mod_mask();
+
+        this.alt_label.sensitive = !isAlt(keyvalLower) && (realMask & Gdk.ModifierType.MOD1_MASK);
+        this.ctrl_label.sensitive = !isCtrl(keyvalLower) && (realMask & Gdk.ModifierType.CONTROL_MASK);
+        this.shift_label.sensitive = !isShift(keyvalLower) && (realMask & Gdk.ModifierType.SHIFT_MASK);
+        this.super_label.sensitive = !isSuper(keyvalLower) && (realMask & Gdk.ModifierType.SUPER_MASK);
+
+        return super.vfunc_key_release_event(event);
+    }
+
+    vfunc_key_press_event(event) {
+        if (!this.plugin.state) {
+            return true;
+        }
+
+        let keyvalLower = Gdk.keyval_to_lower(event.keyval);
+        let realMask = event.state & Gtk.accelerator_get_default_mod_mask();
+
+        this.alt_label.sensitive = isAlt(keyvalLower) || (realMask & Gdk.ModifierType.MOD1_MASK);
+        this.ctrl_label.sensitive = isCtrl(keyvalLower) || (realMask & Gdk.ModifierType.CONTROL_MASK);
+        this.shift_label.sensitive = isShift(keyvalLower) || (realMask & Gdk.ModifierType.SHIFT_MASK);
+        this.super_label.sensitive = isSuper(keyvalLower) || (realMask & Gdk.ModifierType.SUPER_MASK);
+
+        // Wait for a real key before sending
+        if (MOD_KEYS.includes(keyvalLower)) {
+            return false;
+        }
+
+        // Normalize Tab
+        if (keyvalLower === Gdk.KEY_ISO_Left_Tab) {
+            keyvalLower = Gdk.KEY_Tab;
+        }
+
+        // Put shift back if it changed the case of the key, not otherwise.
+        if (keyvalLower !== event.keyval) {
+            realMask |= Gdk.ModifierType.SHIFT_MASK;
+        }
+
+        // HACK: we don't want to use SysRq as a keybinding (but we do want
+        // Alt+Print), so we avoid translation from Alt+Print to SysRq
+        if (keyvalLower === Gdk.KEY_Sys_Req && (realMask & Gdk.ModifierType.MOD1_MASK) !== 0) {
+            keyvalLower = Gdk.KEY_Print;
+        }
+
+        // CapsLock isn't supported as a keybinding modifier, so keep it from
+        // confusing us
+        realMask &= ~Gdk.ModifierType.LOCK_MASK;
+
+        if (keyvalLower !== 0) {
+            debug(`keyval: ${event.keyval}, mask: ${realMask}`);
+
+            let request = {
+                alt: !!(realMask & Gdk.ModifierType.MOD1_MASK),
+                ctrl: !!(realMask & Gdk.ModifierType.CONTROL_MASK),
+                shift: !!(realMask & Gdk.ModifierType.SHIFT_MASK),
+                super: !!(realMask & Gdk.ModifierType.SUPER_MASK),
+                sendAck: true
+            };
+
+            // specialKey
+            if (ReverseKeyMap.has(event.keyval)) {
+                request.specialKey = ReverseKeyMap.get(event.keyval);
+
+            // key
+            } else {
+                let codePoint = Gdk.keyval_to_unicode(event.keyval);
+                request.key = String.fromCodePoint(codePoint);
+            }
+
+            this.device.sendPacket({
+                type: 'kdeconnect.mousepad.request',
+                body: request
+            });
+
+            // Pass these key combinations rather than using the echo reply
+            if (request.alt || request.ctrl || request.super) {
+                return super.vfunc_key_press_event(event);
+            }
+        }
+
+        return false;
+    }
+
+    vfunc_window_state_event(event) {
+        if (this.plugin.state && !!(event.new_window_state & Gdk.WindowState.FOCUSED)) {
+            this._grab();
+        } else {
+            this._ungrab();
+        }
+
+        return super.vfunc_window_state_event(event);
+    }
+
+    _onState(widget) {
+        if (this.plugin.state && this.is_active) {
+            this._grab();
+        } else {
+            this._ungrab();
+        }
+    }
+
+    _grab() {
+        if (!this.visible || this._device) {
+            return;
+        }
+
+        debug('acquiring grab');
+
+        let seat = Gdk.Display.get_default().get_default_seat();
+        let status = seat.grab(
+            this.get_window(),
+            Gdk.SeatCapabilities.KEYBOARD,
+            false,
+            null,
+            null,
+            null
+        );
+
+        if (status !== Gdk.GrabStatus.SUCCESS) {
+            logWarning('Grabbing keyboard failed');
+            return;
+        }
+
+        this._device = seat.get_keyboard();
+        this.grab_add();
+        this.text.has_focus = true;
+    }
+
+    _ungrab() {
+        if (this._device) {
+            debug('releasing grab');
+
+            this._device.get_seat().ungrab();
+            this._device = null;
+            this.grab_remove();
+        }
+
+        this.text.buffer.text = '';
+    }
+});
 
