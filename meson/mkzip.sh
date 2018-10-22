6ffdff48103f43c104daad1f61dc84aa429fd504
Andy Holmes <andrew.g.r.holmes@gmail.com>
2018-10-21 17:43:04 -0700

    meson: add target for building self-extracting archives

--- meson/mkzip.sh
+++ meson/mkzip.sh
@@ -4,8 +4,18 @@ ZIP_DESTDIR="${MESON_BUILD_ROOT}/_zip"
 ZIP_DIR="${MESON_BUILD_ROOT}/${UUID}"
 ZIP_FILE="${MESON_BUILD_ROOT}/${UUID}.zip"
 
+SFX_FILE="${MESON_BUILD_ROOT}/${UUID}.sfx"
+
 GSCHEMA_DIR="${ZIP_DESTDIR}/${GSCHEMADIR}"
 LOCALE_DIR="${ZIP_DESTDIR}/${LOCALEDIR}"
+
+SFX_CMD="#!/bin/sh
+sed -e '1,/^exit$/d' \"\$0\" > /tmp/gsconnect.zip
+mkdir -p ~/.local/share/gnome-shell/extensions
+rm -rf ~/.local/share/gnome-shell/extensions/gsconnect@andyholmes.github.io
+unzip /tmp/gsconnect.zip -d ~/.local/share/gnome-shell/extensions/gsconnect@andyholmes.github.io
+rm /tmp/gsconnect.zip
+exit"
 
 # PRE-CLEAN
 rm -rf ${ZIP_DESTDIR} ${ZIP_DIR} ${ZIP_FILE}
@@ -29,10 +39,24 @@ fi
 cd ${ZIP_DIR}
 zip -qr ${ZIP_FILE} .
 
+echo
+echo "Extension saved to ${ZIP_FILE}"
+
+# SELF EXTRACTOR
+if [ "$SFX" = true ]; then
+    echo "$SFX_CMD" > "${SFX_FILE}"
+    cat ${ZIP_FILE} >> "${SFX_FILE}"
+    chmod a+x ${SFX_FILE}
+
+    echo "Installer saved to $SFX_FILE"
+fi
+
 # INSTALL
 if [ "$INSTALL" = true ]; then
     INSTALL_DIR="${HOME}/.local/share/gnome-shell/extensions/${UUID}"
     rm -rf ${INSTALL_DIR}
     unzip ${ZIP_FILE} -d ${INSTALL_DIR}
+
+    echo "Extension installed to $INSTALL_DIR"
 fi
 
