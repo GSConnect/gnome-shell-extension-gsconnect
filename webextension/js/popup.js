b6b4ccd4039efffa4e170c2d67773b48f2d763ae
Andy Holmes <andrew.g.r.holmes@gmail.com>
2018-11-21 12:52:17 -0800

    WebExtension: general cleanup
    * update webextension-polyfill to 0.3.1
    * first-run lint of background.js and popup.js
    * popup.js: fix incorrect translation message reference
    
    Signed-off-by: Andy Holmes <andrew.g.r.holmes@gmail.com>

--- webextension/js/popup.js
+++ webextension/js/popup.js
@@ -7,26 +7,26 @@ function logError(error) {
     // Suppress errors caused by Mozilla polyfill
     // TODO: Fix these somehow?
     if (
-        error.message !== "Could not establish connection. Receiving end does not exist." &&
-        error.message !== "The message port closed before a response was received."
+        error.message !== 'Could not establish connection. Receiving end does not exist.' &&
+        error.message !== 'The message port closed before a response was received.'
     ) {
-        console.error(error.message)
+        console.error(error.message);
     }
-};
+}
 
 
 function sendMessage(message) {
     browser.runtime.sendMessage(message).then(() => {
         return true;
     }).catch(logError);
-};
+}
 
 
 function sendUrl(device, url) {
-    let [id, action] = device.split(":");
+    let [id, action] = device.split(':');
     
     sendMessage({
-        type: "share",
+        type: 'share',
         data: {
             device: id,
             url: url,
@@ -34,56 +34,56 @@ function sendUrl(device, url) {
         }
     });
     window.close();
-};
+}
 
 
 function sendUrlCallback(target, url) {
-    return () => { sendUrl(target, url); };
-};
+    return () => sendUrl(target, url);
+}
 
 
 function addDevice(device) {
-    let deviceElement = document.createElement("div");
-    deviceElement.className = "device";
+    let deviceElement = document.createElement('div');
+    deviceElement.className = 'device';
     
-    let deviceIcon = document.createElement("img");
-    deviceIcon.className = "device-icon";
-    deviceIcon.src = "images/" + device.type + ".svg";
+    let deviceIcon = document.createElement('img');
+    deviceIcon.className = 'device-icon';
+    deviceIcon.src = 'images/' + device.type + '.svg';
     deviceElement.appendChild(deviceIcon);
     
-    let deviceName = document.createElement("span");
-    deviceName.className = "device-name";
+    let deviceName = document.createElement('span');
+    deviceName.className = 'device-name';
     deviceName.textContent = device.name;
     deviceElement.appendChild(deviceName);
     
     if (device.share) {
-        let shareButton = document.createElement("img");
-        shareButton.className = "plugin-button";
-        shareButton.src = "images/open-in-browser.svg";
-        shareButton.title = browser.i18n.getMessage("shareMessage");
+        let shareButton = document.createElement('img');
+        shareButton.className = 'plugin-button';
+        shareButton.src = 'images/open-in-browser.svg';
+        shareButton.title = browser.i18n.getMessage('shareMessage');
         shareButton.addEventListener(
-            "click", sendUrlCallback(device.id + ":share", currentUrl)
+            'click', sendUrlCallback(device.id + ':share', currentUrl)
         );
         deviceElement.appendChild(shareButton);
     }
     
     if (device.telephony) {
-        let telephonyButton = document.createElement("img");
-        telephonyButton.className = "plugin-button";
-        telephonyButton.src = "images/message.svg";
-        telephonyButton.title = browser.i18n.getMessage("telephonyMessage");
+        let telephonyButton = document.createElement('img');
+        telephonyButton.className = 'plugin-button';
+        telephonyButton.src = 'images/message.svg';
+        telephonyButton.title = browser.i18n.getMessage('smsMessage');
         telephonyButton.addEventListener(
-            "click", sendUrlCallback(device.id + ":telephony", currentUrl)
+            'click', sendUrlCallback(device.id + ':telephony', currentUrl)
         );
         deviceElement.appendChild(telephonyButton);
     }
     
     return deviceElement;
-};
+}
 
 
 function setPopup() {
-    let devNode = document.getElementById("popup");
+    let devNode = document.getElementById('popup');
     
     while (devNode.hasChildNodes()) {
         devNode.removeChild(devNode.lastChild);
@@ -91,30 +91,30 @@ function setPopup() {
     
     // The native messaging host is disconnected, or we're disconnected from it
     if (!connected) {
-        let noDevices = document.createElement("span");
-        noDevices.className = "popup-menu-disconnected";
-        noDevices.textContent = browser.i18n.getMessage("popupMenuDisconnected");
+        let noDevices = document.createElement('span');
+        noDevices.className = 'popup-menu-disconnected';
+        noDevices.textContent = browser.i18n.getMessage('popupMenuDisconnected');
         devNode.appendChild(noDevices);
     } else if (devices.length) {
         for (let device of devices) {
             devNode.appendChild(addDevice(device));
         }
     } else {
-        let noDevices = document.createElement("span");
-        noDevices.className = "popup-menu-no-devices";
-        noDevices.textContent = browser.i18n.getMessage("popupMenuNoDevices");
+        let noDevices = document.createElement('span');
+        noDevices.className = 'popup-menu-no-devices';
+        noDevices.textContent = browser.i18n.getMessage('popupMenuNoDevices');
         devNode.appendChild(noDevices);
     }
-};
+}
 
 
 function onMessage(message, sender) {
-    console.log("onMessage: " + JSON.stringify(message));
+    console.log('onMessage: ' + JSON.stringify(message));
     
-    if (sender.url.indexOf("/background.html") > -1) {
-        if (message.type === "connected") {
+    if (sender.url.indexOf('/background.html') > -1) {
+        if (message.type === 'connected') {
             connected = message.data;
-        } else if (message.type === "devices") {
+        } else if (message.type === 'devices') {
             connected = true;
             devices = message.data;
         }
@@ -123,21 +123,21 @@ function onMessage(message, sender) {
     }
     
     return Promise.resolve();
-};
+}
 
 
 function getCurrentTab(callback) {
-    browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+    browser.tabs.query({active: true, currentWindow: true}).then((tabs) => {
         if (tabs.length) {
             callback(tabs[0]);
         }
     });
-};
+}
 
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
     setPopup();
-    sendMessage({ type: "devices" });
+    sendMessage({type: 'devices'});
     
     getCurrentTab((tab) => {
         if (tab) {
