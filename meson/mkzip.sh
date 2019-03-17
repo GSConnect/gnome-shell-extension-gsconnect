1de25671a899f6b04757b7844ff64a877535934e
Andy Holmes <andrew.g.r.holmes@gmail.com>
2019-03-17 15:45:56 -0700

    meson: remove unused sfx target

--- meson/mkzip.sh
+++ meson/mkzip.sh
@@ -35,35 +35,6 @@ rm -rf _zip
 echo
 echo "Extension saved to ${ZIP_FILE}"
 
-# SELF EXTRACTOR
-if [ "$SFX" = true ]; then
-
-    SFX_CMD="\
-#!/bin/sh
-sed -e '1,/^exit$/d' \"\$0\" > /tmp/gsconnect.zip
-
-if [ \"$(sha512sum ${ZIP_FILE} | cut -c -128)\" = \$(sha512sum /tmp/gsconnect.zip | cut -c -128) ]; then
-    printf 'Installing...'
-
-    mkdir -p ~/.local/share/gnome-shell/extensions
-    rm -rf ~/.local/share/gnome-shell/extensions/gsconnect@andyholmes.github.io
-    unzip /tmp/gsconnect.zip -d ~/.local/share/gnome-shell/extensions/gsconnect@andyholmes.github.io > /dev/null
-    rm /tmp/gsconnect.zip
-
-    echo 'done'
-else
-    echo 'Error: Checksum mismatch. Please download the archive again.'
-fi
-
-exit"
-
-    echo "$SFX_CMD" > "${SFX_FILE}"
-    cat ${ZIP_FILE} >> "${SFX_FILE}"
-    chmod a+x ${SFX_FILE}
-
-    echo "Installer saved to $SFX_FILE"
-fi
-
 # INSTALL
 if [ "$INSTALL" = true ]; then
     EXTENSIONS_DIR="${HOME}/.local/share/gnome-shell/extensions"
