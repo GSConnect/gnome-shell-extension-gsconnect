b3167503b742d35a71407147dad3ec9016132e1b
Andy Holmes <andrew.g.r.holmes@gmail.com>
2019-06-21 17:14:50 -0700

    Clipboard Component: improvements to XWayland clipboard proxy
    * clarify process pipe names
    * watch stderr for errors
    * cleanup `xclipboard` and comments
    
    addresses #454

--- src/service/components/clipboard.js
+++ src/service/components/clipboard.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Gdk = imports.gi.Gdk;
+const GLib = imports.gi.GLib;
 const Gtk = imports.gi.Gtk;
 const Gio = imports.gi.Gio;
 const GObject = imports.gi.GObject;
@@ -30,24 +31,36 @@ var Clipboard = GObject.registerClass({
                     argv: [gsconnect.extdatadir + '/service/components/xclipboard'],
                     flags: Gio.SubprocessFlags.STDIN_PIPE |
                            Gio.SubprocessFlags.STDOUT_PIPE |
-                           Gio.SubprocessFlags.STDERR_SILENCE
+                           Gio.SubprocessFlags.STDERR_PIPE
                 });
                 this._proc.init(null);
                 
                 // IO Channels
-                this._stdin = new Gio.DataInputStream({
+                this._stdin = new Gio.DataOutputStream({
+                    base_stream: this._proc.get_stdin_pipe(),
+                    byte_order: Gio.DataStreamByteOrder.HOST_ENDIAN
+                });
+                
+                this._stdout = new Gio.DataInputStream({
                     base_stream: this._proc.get_stdout_pipe(),
                     byte_order: Gio.DataStreamByteOrder.HOST_ENDIAN
                 });
 
-                this._stdout = new Gio.DataOutputStream({
-                    base_stream: this._proc.get_stdin_pipe(),
+                this._stderr = new Gio.DataInputStream({
+                    base_stream: this._proc.get_stderr_pipe(),
                     byte_order: Gio.DataStreamByteOrder.HOST_ENDIAN
                 });
+                
+                // Watch for premature exits
+                this._proc.wait_check_async(null, this._procExit.bind(this));
 
-                let source = this._stdin.base_stream.create_source(null);
+                // Watch for clipboard content
+                let source = this._stdout.base_stream.create_source(null);
                 source.set_callback(this._readContent.bind(this));
                 source.attach(null);
+                
+                // Watch for errors
+                this._readError(this._stderr);
                 
             // If we're in X11/Xorg we're just a wrapper around GtkClipboard
             } else {
@@ -71,8 +84,8 @@ var Clipboard = GObject.registerClass({
     _readContent() {
         try {
             // Read the message
-            let length = this._stdin.read_int32(null);
-            let text = this._stdin.read_bytes(length, null).toArray();
+            let length = this._stdout.read_int32(null);
+            let text = this._stdout.read_bytes(length, null).toArray();
 
             if (text instanceof Uint8Array) {
                 text = imports.byteArray.toString(text);
@@ -83,16 +96,53 @@ var Clipboard = GObject.registerClass({
 
             return true;
         } catch (e) {
-            logError(e);
+            // Silence errors; we get our errors from stderr directly
         }
     }
     
+    _readError(stderr) {
+        stderr.read_line_async(GLib.PRIORITY_DEFAULT, null, (stream, res) => {
+            try {
+                let line = stream.read_line_finish_utf8(res)[0];
+                
+                if (line !== null) {
+                    debug(`XClipboard: ${line}`);
+                    this._readError(stream);
+                }
+            } catch (e) {
+                // Silence errors; we get our errors from stderr directly
+            }
+        });
+    }
+    
     _writeContent(text) {
+        // Bail if xclipboard failed
+        if (!this._proc) {
+            warning('XClipboard not running');
+            return;
+        }
+        
         try {
-            this._stdout.put_int32(text.length, null);
-            this._stdout.put_string(text, null);
+            this._stdin.put_int32(text.length, null);
+            this._stdin.put_string(text, null);
         } catch (e) {
-            logError(e, 'Clipboard');
+            debug(e, 'XClipboard');
+        }
+    }
+    
+    _procExit(proc, res) {
+        try {
+            this._proc = null;
+            this._stdin.close(null);
+            this._stdin = null;
+            this._stdout.close(null);
+            this._stdout = null;
+            this._stderr.close(null);
+            this._stderr = null;
+            
+            proc.wait_check_finish(res);
+        } catch (e) {
+            debug(e, 'XClipboard');
         }
     }
     
@@ -122,7 +172,7 @@ var Clipboard = GObject.registerClass({
     
     destroy() {
         if (this._proc) {
-            this._proc.force_exit(null);
+            this._proc.force_exit();
         }
         
         if (this._ownerChangeId) {
