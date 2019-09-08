e72703fbbe1710f42617587326bacf7ccb096945
Andy Holmes <andrew.g.r.holmes@gmail.com>
2019-09-08 16:46:02 -0700

    FindMyPhone: add `sound` component and make dialog easier to close
    closes #573

---
+++ src/service/components/sound.js
@@ -0,0 +1,201 @@
+'use strict';
+
+const Gdk = imports.gi.Gdk;
+const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
+const GObject = imports.gi.GObject;
+
+
+/**
+ * Used to ensure 'audible-bell' is enabled for fallback
+ */
+const WM_SETTINGS = new Gio.Settings({
+    schema_id: 'org.gnome.desktop.wm.preferences',
+    path: '/org/gnome/desktop/wm/preferences/'
+});
+
+
+var Sound = class Sound {
+
+    constructor() {
+    }
+
+    get backend() {
+        if (this._backend === undefined) {
+            // Prefer GSound
+            try {
+                this._gsound = new imports.gi.GSound.Context();
+                this._gsound.init(null);
+                this._backend = 'gsound';
+
+            // Try falling back to libcanberra, otherwise just re-run the test
+            // in case one or the other is installed later
+            } catch (e) {
+                if (GLib.find_program_in_path('canberra-gtk-play') !== null) {
+                    this._canberra = new Gio.SubprocessLauncher({
+                        flags: Gio.SubprocessFlags.NONE
+                    });
+                    this._backend = 'libcanberra';
+                } else {
+                    return null;
+                }
+            }
+        }
+
+        return this._backend;
+    }
+
+    get playing() {
+        if (this._playing === undefined) {
+            this._playing = new Set();
+        }
+
+        return this._playing;
+    }
+
+    _canberraPlaySound(name, cancellable) {
+        return new Promise((resolve, reject) => {
+            let proc = this._canberra.spawnv(['canberra-gtk-play', '-i', name]);
+
+            proc.wait_check_async(cancellable, (proc, res) => {
+                try {
+                    resolve(proc.wait_check_finish(res));
+                } catch (e) {
+                    reject(e);
+                }
+            });
+        });
+    }
+
+    async _canberraLoopSound(name, cancellable) {
+        while (!cancellable.is_cancelled()) {
+            await this._canberraPlaySound(name, cancellable);
+        }
+    }
+
+    _gsoundPlaySound(name, cancellable) {
+        return new Promise((resolve, reject) => {
+            this._gsound.play_full(
+                {'event.id': name},
+                cancellable,
+                (source, res) => {
+                    try {
+                        resolve(source.play_full_finish(res));
+                    } catch (e) {
+                        reject(e);
+                    }
+                }
+            );
+        });
+    }
+
+    async _gsoundLoopSound(name, cancellable) {
+        while (!cancellable.is_cancelled()) {
+            await this._gsoundPlaySound(name, cancellable);
+        }
+    }
+
+    _gdkPlaySound(name, cancellable) {
+        if (this._display === undefined) {
+            this._display = Gdk.Display.get_default();
+        }
+
+        let count = 0;
+
+        GLib.timeout_add(GLib.PRIORITY_DEFAULT, 200, () => {
+            try {
+                if (count++ < 4 && !cancellable.is_cancelled()) {
+                    this._display.beep();
+                    return GLib.SOURCE_CONTINUE;
+                }
+
+                return GLib.SOURCE_REMOVE;
+            } catch (e) {
+                logError(e);
+                return GLib.SOURCE_REMOVE;
+            }
+        });
+
+        return !cancellable.is_cancelled();
+    }
+
+    _gdkLoopSound(name, cancellable) {
+        this._gdkPlaySound(name, cancellable);
+        GLib.timeout_add(
+            GLib.PRIORITY_DEFAULT,
+            1500,
+            this._gdkPlaySound.bind(this, name, cancellable)
+        );
+    }
+
+    async playSound(name, cancellable) {
+        try {
+            if (!(cancellable instanceof Gio.Cancellable)) {
+                cancellable = new Gio.Cancellable();
+            }
+
+            this.playing.add(cancellable);
+
+            switch (this.backend) {
+                case 'gsound':
+                    await this._gsoundPlaySound(name, cancellable);
+                    break;
+
+                case 'canberra':
+                    await this._canberraPlaySound(name, cancellable);
+                    break;
+
+                default:
+                    await this._gdkPlaySound(name, cancellable);
+            }
+        } catch (e) {
+            if (!e.code || e.code !== Gio.IOErrorEnum.CANCELLED) {
+                logError(e);
+            }
+        } finally {
+            this.playing.delete(cancellable);
+        }
+    }
+
+    async loopSound(name, cancellable) {
+        try {
+            if (!(cancellable instanceof Gio.Cancellable)) {
+                cancellable = new Gio.Cancellable();
+            }
+
+            this.playing.add(cancellable);
+
+            switch (this.backend) {
+                case 'gsound':
+                    await this._gsoundLoopSound(name, cancellable);
+                    break;
+
+                case 'canberra':
+                    await this._canberraLoopSound(name, cancellable);
+                    break;
+
+                default:
+                    await this._gdkLoopSound(name, cancellable);
+            }
+        } catch (e) {
+            if (!e.code || e.code !== Gio.IOErrorEnum.CANCELLED) {
+                logError(e);
+            }
+        } finally {
+            this.playing.delete(cancellable);
+        }
+    }
+
+    destroy() {
+        for (let cancellable of this.playing) {
+            cancellable.cancel();
+        }
+    }
+};
+
+
+/**
+ * The service class for this component
+ */
+var Component = Sound;
+
--- src/service/plugins/findmyphone.js
+++ src/service/plugins/findmyphone.js
@@ -90,33 +90,6 @@ var Plugin = GObject.registerClass({
 
 
 /**
- * Return the backend to be used for playing sound effects
- *
- * @return {string|boolean} - 'gsound', 'libcanberra' or %false
- */
-function get_backend() {
-    if (window._SFX_BACKEND) {
-        return _SFX_BACKEND;
-    }
-
-    // Service-wide GSound.Context singleton
-    try {
-        window._GSOUND_CONTEXT = new imports.gi.GSound.Context();
-        window._GSOUND_CONTEXT.init(null);
-        window._SFX_BACKEND = 'gsound';
-
-    // Try falling back to libcanberra
-    } catch (e) {
-        if (GLib.find_program_in_path('canberra-gtk-play') !== null) {
-            window._SFX_BACKEND = 'libcanberra';
-        }
-    }
-
-    return _SFX_BACKEND;
-}
-
-
-/**
  * Used to ensure 'audible-bell' is enabled for fallback
  */
 const WM_SETTINGS = new Gio.Settings({
@@ -136,118 +109,85 @@ const Dialog = GObject.registerClass({
             buttons: Gtk.ButtonsType.CLOSE,
             image: new Gtk.Image({
                 icon_name: 'phonelink-ring-symbolic',
-                pixel_size: 128
+                pixel_size: 512,
+                halign: Gtk.Align.CENTER,
+                hexpand: true,
+                valign: Gtk.Align.CENTER,
+                vexpand: true,
+                visible: true
             }),
-            urgency_hint: true,
-            window_position: Gtk.WindowPosition.CENTER_ALWAYS
+            urgency_hint: true
         });
 
         this.set_keep_above(true);
+        this.maximize();
         this.message_area.destroy();
 
-        // Ensure the volume is sufficient
-        let mixer = Gio.Application.get_default().pulseaudio;
+        // If the mixer is available start fading the volume up
+        let service = Gio.Application.get_default();
+        let mixer = service.components.get('pulseaudio');
 
         if (mixer) {
             this._stream = mixer.output;
-            this._previousVolume = this._stream.volume;
+
             this._previousMuted = this._stream.muted;
-            this._stream.volume = 0.85;
+            this._previousVolume = this._stream.volume;
+
             this._stream.muted = false;
+            this._stream.fade(0.85, 15);
+
+        // Otherwise ensure audible-bell is enabled
+        } else {
+            this._previousBell = WM_SETTINGS.get_boolean('audible-bell');
+            WM_SETTINGS.set_boolean('audible-bell', true);
         }
 
-        // Ensure audible-bell is enabled for fallback
-        this._previousBell = WM_SETTINGS.get_boolean('audible-bell');
-        WM_SETTINGS.set_boolean('audible-bell', true);
+        // Start the alarm
+        let sound = service.components.get('sound');
 
-        // Show the dialog and start the alarm
+        if (sound !== undefined) {
+            sound.loopSound('phone-incoming-call', this.cancellable);
+        }
+
+        // Show the dialog
         this.show_all();
-        this._cancellable = new Gio.Cancellable();
-        this.bell();
+    }
+
+    vfunc_key_press_event(event) {
+        this.response(Gtk.ResponseType.DELETE_EVENT);
+
+        return Gdk.EVENT_STOP;
+    }
+
+    vfunc_motion_notify_event(event) {
+        this.response(Gtk.ResponseType.DELETE_EVENT);
+
+        return Gdk.EVENT_STOP;
     }
 
     vfunc_response(response_id) {
         // Stop the alarm
-        this._cancellable.cancel();
+        this.cancellable.cancel();
 
         // Restore the mixer level
         if (this._stream) {
-            this._stream.volume = this._previousVolume;
             this._stream.muted = this._previousMuted;
-        }
+            this._stream.fade(this._previousVolume);
 
         // Restore the audible-bell
-        WM_SETTINGS.set_boolean('audible-bell', this._previousBell);
+        } else {
+            WM_SETTINGS.set_boolean('audible-bell', this._previousBell);
+        }
 
         this.destroy();
     }
 
-    bell() {
-        let proc;
-
-        switch (get_backend()) {
-            case 'gsound':
-                _GSOUND_CONTEXT.play_full(
-                    {'event.id': 'phone-incoming-call'},
-                    this._cancellable,
-                    (source, res) => {
-                        try {
-                            source.play_full_finish(res);
-                            this.bell();
-                        } catch (e) {
-                        }
-                    }
-                );
-                break;
-
-            case 'libcanberra':
-                proc = new Gio.Subprocess({
-                    argv: ['canberra-gtk-play', '-i', 'phone-incoming-call'],
-                    flags: Gio.SubprocessFlags.NONE
-                });
-                proc.init(null);
-
-                proc.wait_check_async(this._cancellable, (proc, res) => {
-                    try {
-                        proc.wait_check_finish(res);
-                        this.bell();
-                    } catch (e) {
-                    }
-                });
-                break;
-
-            default:
-                this._display = Gdk.Display.get_default();
-                this._fallback();
-                GLib.timeout_add(
-                    GLib.PRIORITY_DEFAULT,
-                    1500,
-                    () => this._fallback()
-                );
+    get cancellable() {
+        if (this._cancellable === undefined) {
+            this._cancellable = new Gio.Cancellable();
         }
-    }
 
-    /**
-     * A fallback for playing an alert using gdk_display_bell() when neither
-     * GSound nor canberra-gtk-play are available.
-     */
-    _fallback() {
-        let count = 0;
-
-        GLib.timeout_add(GLib.PRIORITY_DEFAULT, 200, () => {
-            try {
-                if (count++ < 4 && !this._cancellable.is_cancelled()) {
-                    this._display.beep();
-                    return GLib.SOURCE_CONTINUE;
-                }
-
-                return GLib.SOURCE_REMOVE;
-            } catch (e) {
-                return GLib.SOURCE_REMOVE;
-            }
-        });
-
-        return !this._cancellable.is_cancelled();
+        return this._cancellable;
     }
 });
 
