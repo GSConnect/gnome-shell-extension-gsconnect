45f47908721f7fb56e6499469ad576a390f2add1
Andy Holmes <andrew.g.r.holmes@gmail.com>
2019-10-22 16:58:46 -0700

    Input Component: set a 15s timeout for RemoteDesktop sessions
    Start the Mutter RemoteDesktop session on demand, and timeout after 15s
    of inactivity instead of always keeping a session open.
    
    closes #671

--- src/service/components/input.js
+++ src/service/components/input.js
@@ -6,6 +6,9 @@ const GLib = imports.gi.GLib;
 const GObject = imports.gi.GObject;
 
 
+const SESSION_TIMEOUT = 15;
+
+
 const RemoteSession = GObject.registerClass({
     GTypeName: 'GSConnectRemoteSession',
     Implements: [Gio.DBusInterface],
@@ -32,6 +35,8 @@ const RemoteSession = GObject.registerClass({
             g_object_path: objectPath,
             g_interface_name: 'org.gnome.Mutter.RemoteDesktop.Session'
         });
+
+        this._started = false;
     }
 
     vfunc_g_signal(sender_name, signal_name, parameters) {
@@ -49,6 +54,8 @@ const RemoteSession = GObject.registerClass({
     }
 
     _call(name, parameters = null) {
+        if (!this._started) return;
+
         this.call(
             name,
             parameters,
@@ -68,6 +75,8 @@ const RemoteSession = GObject.registerClass({
 
     async start() {
         try {
+            if (this._started) return;
+
             // Initialize the proxy
             await new Promise((resolve, reject) => {
                 this.init_async(
@@ -78,7 +87,6 @@ const RemoteSession = GObject.registerClass({
                             proxy.init_finish(res);
                             resolve();
                         } catch (e) {
-                            Gio.DBusError.strip_remote_error(e);
                             reject(e);
                         }
                     }
@@ -97,15 +105,25 @@ const RemoteSession = GObject.registerClass({
                         try {
                             resolve(proxy.call_finish(res));
                         } catch (e) {
-                            Gio.DBusError.strip_remote_error(e);
                             reject(e);
                         }
                     }
                 );
             });
+
+            this._started = true;
         } catch (e) {
             this.destroy();
+
+            Gio.DBusError.strip_remote_error(e);
             throw e;
+        }
+    }
+
+    stop() {
+        if (this._started) {
+            this._started = false;
+            this.call('Stop', null, Gio.DBusCallFlags.NONE, -1, null, null);
         }
     }
 
@@ -262,67 +280,40 @@ const RemoteSession = GObject.registerClass({
 
 const Controller = class Controller {
     constructor() {
-        this._controller = null;
+        this._adapter = null;
         this._nameAppearedId = 0;
         this._sessionCloseId = 0;
+        this._sessionExpiry = 0;
+        this._sessionExpiryId = 0;
+        this._sessionStarting = false;
 
-        this._prepareController();
-    }
-
-    async _prepareController() {
-        try {
-            // Check for Mutter's remote desktop interface
-            if (this._mutter === undefined) {
-                let names = await this._listNames();
-                this._mutter = names.includes('org.gnome.Mutter.RemoteDesktop');
-            }
-
-            // Prefer the newer remote desktop interface
-            if (this._mutter) {
-                debug('Using Mutter RemoteDesktop');
-
-                if (this._nameAppearedId === 0) {
-                    this._nameAppearedId = Gio.bus_watch_name_on_connection(
-                        this.connection,
-                        'org.gnome.Mutter.RemoteDesktop',
-                        Gio.BusNameWatcherFlags.NONE,
-                        this._onNameAppeared.bind(this),
-                        this._onNameVanished.bind(this)
-                    );
-                }
-
-            // Fallback to Atspi
-            } else {
-                debug('Falling back to Atspi');
-
-                let fallback = imports.service.components.atspi;
-                this._controller = new fallback.Controller();
-            }
-        } catch (e) {
-            this._mutter = false;
-            debug(e);
-        }
+        // Watch for the RemoteDesktop portal
+        this._nameWatcherId = Gio.bus_watch_name(
+            Gio.BusType.SESSION,
+            'org.gnome.Mutter.RemoteDesktop',
+            Gio.BusNameWatcherFlags.NONE,
+            this._onNameAppeared.bind(this),
+            this._onNameVanished.bind(this)
+        );
     }
 
     get connection() {
         if (this._connection === undefined) {
-            let service = Gio.Application.get_default();
-            this._connection = service.get_dbus_connection();
+            this._connection = null;
         }
 
         return this._connection;
     }
 
-    async _onNameAppeared(connection, name, name_owner) {
+    _onNameAppeared(connection, name, name_owner) {
         try {
-            let objectPath = await this._createSession();
-            this._controller = new RemoteSession(objectPath);
-            this._controller.start();
+            // Destroy any old Atspi adapter
+            if (!this.connection && this._adapter) {
+                this._adapter.destroy();
+                this._adapter = null;
+            }
 
-            this._sessionClosedId = this._controller.connect(
-                'closed',
-                this._onSessionClosed.bind(this)
-            );
+            this._connection = connection;
         } catch (e) {
             logError(e);
         }
@@ -330,48 +321,71 @@ const Controller = class Controller {
 
     _onNameVanished(connection, name) {
         try {
-            // Ensure we're disconnected from the session
-            if (this._controller && this._sessionClosedId > 0) {
-                this._controller.disconnect(this._sessionClosedId);
-                this._sessionClosedId = 0;
+            // Destroy any old RemoteDesktop session
+            if (this._adapter instanceof RemoteSession) {
+                // Disconnect from the session
+                if (this._sessionClosedId > 0) {
+                    this._adapter.disconnect(this._sessionClosedId);
+                    this._sessionClosedId = 0;
+                }
+
+                // Destroy the session
+                this._adapter.destroy();
+                this._adapter = null;
             }
 
-            // Destroy the session
-            if (this._controller) {
-                this._controller.destroy();
-                this._controller = null;
-            }
+            this._connection = null;
         } catch (e) {
             logError(e);
         }
     }
 
-    _listNames() {
-        return new Promise((resolve, reject) => {
-            this.connection.call(
-                'org.freedesktop.DBus',
-                '/org/freedesktop/DBus',
-                'org.freedesktop.DBus',
-                'ListNames',
-                null,
-                null,
-                Gio.DBusCallFlags.NONE,
-                -1,
-                null,
-                (connection, res) => {
-                    try {
-                        res = connection.call_finish(res);
-                        resolve(res.deep_unpack()[0]);
-                    } catch (e) {
-                        reject(e);
-                    }
-                }
+    _onSessionClosed(session) {
+        // Disconnect from the session
+        if (this._sessionClosedId > 0) {
+            session.disconnect(this._sessionClosedId);
+            this._sessionClosedId = 0;
+        }
+
+        // Destroy the session
+        session.destroy();
+        this._adapter = null;
+    }
+
+    _onSessionExpired() {
+        // If the session has been used recently, schedule a new expiry
+        let remainder = Math.floor(this._sessionExpiry - (Date.now() / 1000));
+
+        if (remainder > 0) {
+            debug('rescheduling expiry');
+            this._sessionExpiryId = GLib.timeout_add_seconds(
+                GLib.PRIORITY_DEFAULT,
+                remainder,
+                this._onSessionExpired.bind(this)
             );
-        });
+
+            return GLib.SOURCE_REMOVE;
+        }
+
+        // If there's a current session, close it
+        debug(`${(this._adapter instanceof RemoteSession)}`);
+        if (this._adapter instanceof RemoteSession) {
+            this._adapter.stop();
+        }
+
+        // Reset the GSource Id
+        this._sessionExpiryId = 0;
+
+        return GLib.SOURCE_REMOVE;
     }
 
     _createSession() {
         return new Promise((resolve, reject) => {
+            if (this.connection === null) {
+                reject(new Error('No DBus connection'));
+                return;
+            }
+
             this.connection.call(
                 'org.gnome.Mutter.RemoteDesktop',
                 '/org/gnome/Mutter/RemoteDesktop',
@@ -394,75 +408,172 @@ const Controller = class Controller {
         });
     }
 
-    // FIXME
-    _onSessionClosed(session) {
-        // Ensure we're disconnected from the session
-        if (this._sessionClosedId > 0) {
-            session.disconnect(this._sessionClosedId);
-            this._sessionClosedId = 0;
+    async _ensureAdapter() {
+        try {
+            // Update the timestamp of the last event
+            this._sessionExpiry = Math.floor((Date.now() / 1000) + SESSION_TIMEOUT);
+
+            // Adapter is ensured
+            if (this._adapter) return;
+
+            // Mutter's RemoteDesktop portal is not available
+            if (!this.connection) {
+                debug('Falling back to Atspi');
+
+                let fallback = imports.service.components.atspi;
+                this._adapter = new fallback.Controller();
+
+            // Mutter is available and there isn't another session starting
+            } else if (this._sessionStarting === false) {
+                debug('Creating Mutter RemoteDesktop session');
+
+                this._sessionStarting = true;
+
+                let objectPath = await this._createSession();
+                this._adapter = new RemoteSession(objectPath);
+                this._adapter.start();
+
+                this._sessionClosedId = this._adapter.connect(
+                    'closed',
+                    this._onSessionClosed.bind(this)
+                );
+
+                if (this._sessionExpiryId === 0) {
+                    this._sessionExpiryId = GLib.timeout_add_seconds(
+                        GLib.PRIORITY_DEFAULT,
+                        SESSION_TIMEOUT,
+                        this._onSessionExpired.bind(this)
+                    );
+                }
+            }
+        } catch (e) {
+            logError(e);
+
+            this._adapter.destroy();
+            this._adapter = null;
+        } finally {
+            this._sessionStarting = false;
         }
-
-        // Destroy the session
-        session.destroy();
-        this._controller = undefined;
-
-        // Prepare a new session
-        this._prepareController();
     }
 
     /**
      * Pointer Events
      */
     movePointer(dx, dy) {
-        this._controller.movePointer(dx, dy);
+        try {
+            if (dx === 0 && dy === 0) return;
+
+            this._ensureAdapter();
+            this._adapter.movePointer(dx, dy);
+        } catch (e) {
+            debug(e);
+        }
     }
 
     pressPointer(button) {
-        this._controller.pressPointer(button);
+        try {
+            this._ensureAdapter();
+            this._adapter.pressPointer(button);
+        } catch (e) {
+            debug(e);
+        }
     }
 
     releasePointer(button) {
-        this._controller.releasePointer(button);
+        try {
+            this._ensureAdapter();
+            this._adapter.releasePointer(button);
+        } catch (e) {
+            debug(e);
+        }
     }
 
     clickPointer(button) {
-        this._controller.clickPointer(button);
+        try {
+            this._ensureAdapter();
+            this._adapter.clickPointer(button);
+        } catch (e) {
+            debug(e);
+        }
     }
 
     doubleclickPointer(button) {
-        this._controller.doubleclickPointer(button);
+        try {
+            this._ensureAdapter();
+            this._adapter.doubleclickPointer(button);
+        } catch (e) {
+            debug(e);
+        }
     }
 
     scrollPointer(dx, dy) {
-        this._controller.scrollPointer(dx, dy);
+        if (dx === 0 && dy === 0) return;
+
+        try {
+            this._ensureAdapter();
+            this._adapter.scrollPointer(dx, dy);
+        } catch (e) {
+            debug(e);
+        }
     }
 
     /**
      * Keyboard Events
      */
     pressKeysym(keysym) {
-        this._controller.pressKeysym(keysym);
+        try {
+            this._ensureAdapter();
+            this._adapter.pressKeysym(keysym);
+        } catch (e) {
+            debug(e);
+        }
     }
 
     releaseKeysym(keysym) {
-        this._controller.releaseKeysym(keysym);
+        try {
+            this._ensureAdapter();
+            this._adapter.releaseKeysym(keysym);
+        } catch (e) {
+            debug(e);
+        }
     }
 
     pressreleaseKeysym(keysym) {
-        this._controller.pressreleaseKeysym(keysym);
+        try {
+            this._ensureAdapter();
+            this._adapter.pressreleaseKeysym(keysym);
+        } catch (e) {
+            debug(e);
+        }
     }
 
     /**
      * High-level keyboard input
      */
     pressKey(input, modifiers) {
-        this._controller.pressKey(input, modifiers);
+        try {
+            this._ensureAdapter();
+            this._adapter.pressKey(input, modifiers);
+        } catch (e) {
+            debug(e);
+        }
     }
 
     destroy() {
-        if (this._controller !== undefined) {
-            this._controller.destroy();
-            this._controller = undefined;
+        if (this._adapter !== null) {
+            // Disconnect from the session
+            if (this._sessionClosedId > 0) {
+                this._adapter.disconnect(this._sessionClosedId);
+                this._sessionClosedId = 0;
+            }
+
+            this._adapter.destroy();
+            this._adapter = null;
+        }
+
+        if (this._nameWatcherId > 0) {
+            Gio.bus_unwatch_name(this._nameWatcherId);
+            this._nameWatcherId = 0;
         }
     }
 };
