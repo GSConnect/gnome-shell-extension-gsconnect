f6720be723c8bcee280a06b253a01e27bd8606db
Andy Holmes <andrew.g.r.holmes@gmail.com>
2019-12-04 23:14:26 -0800

    Contacts Component: replace libfolks with eds
    libfolks is difficult to instrospect, requires pulling in python3 and
    scraping it in a subprocess.
    
    Switching to Evolution Data Server still requires extra GIRs, but is
    much easier to introspect and allows for much easier scraping and
    searching.
    
    closes #718

--- src/service/components/contacts.js
+++ src/service/components/contacts.js
@@ -5,6 +5,15 @@ const Gio = imports.gi.Gio;
 const GLib = imports.gi.GLib;
 const GObject = imports.gi.GObject;
 
+var HAVE_EDS = true;
+
+try {
+    var EBook = imports.gi.EBook;
+    var EBookContacts = imports.gi.EBookContacts;
+    var EDataServer = imports.gi.EDataServer;
+} catch (e) {
+    HAVE_EDS = false;
+}
 
 /**
  * A store for contacts
@@ -43,9 +52,309 @@ var Store = GObject.registerClass({
 
         this.__cache_data = {};
 
-        // Automatically prepare the desktop store
-        if (context === null) {
-            this.load();
+        // If Evolution Data Server is available, load it now
+        if (context === null && HAVE_EDS) {
+            this._ebooks = new Map();
+            this._initEvolutionDataServer();
+        }
+    }
+
+    /**
+     * Parse an EContact and return a small Object
+     *
+     * @param {EBookContacts.Contact} econtact - an EContact to parse
+     * @param {string} [origin] - an optional origin string
+     * @returns {Object} - a small JSON serializable object
+     */
+    async _parseEContact(econtact, origin = 'desktop') {
+        try {
+            let contact = {
+                id: econtact.id,
+                name: _('Unknown Contact'),
+                numbers: [],
+                origin: origin,
+                timestamp: 0
+            };
+
+            // Try to get a contact name
+            if (econtact.full_name)
+                contact.name = econtact.full_name;
+
+            // If there's no phone number, we're not interested
+            let nums = econtact.get_attributes(EBookContacts.ContactField.TEL);
+
+            if (nums.length === 0)
+                return undefined;
+
+            for (let attr of nums) {
+                let number = {
+                    value: attr.get_value(),
+                    type: 'unknown'
+                };
+
+                if (attr.has_type('CELL'))
+                    number.type = 'cell';
+                else if (attr.has_type('HOME'))
+                    number.type = 'home';
+                else if (attr.has_type('WORK'))
+                    number.type = 'work';
+
+                contact.numbers.push(number);
+            }
+
+            // Try and get a contact photo
+            let photo = econtact.photo;
+
+            if (photo) {
+                if (photo.type === EBookContacts.ContactPhotoType.INLINED) {
+                    let data = photo.get_inlined()[0];
+                    contact.avatar = await this.storeAvatar(data);
+
+                } else if (photo.type === EBookContacts.ContactPhotoType.URI) {
+                    let uri = econtact.photo.get_uri();
+                    contact.avatar = uri.replace('file://', '');
+                }
+            }
+
+            return contact;
+        } catch (e) {
+            logError(e, `Failed to parse VCard contact ${econtact.id}`);
+            return undefined;
+        }
+    }
+
+    /*
+     * EDS Helpers
+     */
+    _getEBookClient(source, cancellable = null) {
+        return new Promise((resolve, reject) => {
+            EBook.BookClient.connect(source, 0, cancellable, (source, res) => {
+                try {
+                    resolve(EBook.BookClient.connect_finish(res));
+                } catch (e) {
+                    reject(e);
+                }
+            });
+        });
+    }
+
+    _getEBookView(client, query = '', cancellable = null) {
+        return new Promise((resolve, reject) => {
+            client.get_view(query, cancellable, (client, res) => {
+                try {
+                    resolve(client.get_view_finish(res)[1]);
+                } catch (e) {
+                    reject(e);
+                }
+            });
+        });
+    }
+
+    _getEContacts(client, query = '', cancellable = null) {
+        return new Promise((resolve, reject) => {
+            client.get_contacts('', cancellable, (client, res) => {
+                try {
+                    resolve(client.get_contacts_finish(res)[1]);
+                } catch (e) {
+                    debug(e);
+                    reject([]);
+                }
+            });
+        });
+    }
+
+    _getESourceRegistry(cancellable = null) {
+        return new Promise ((resolve, reject) => {
+            EDataServer.SourceRegistry.new(cancellable, (registry, res) => {
+                try {
+                    resolve(EDataServer.SourceRegistry.new_finish(res));
+                } catch (e) {
+                    reject(e);
+                }
+            });
+        });
+    }
+
+    /*
+     * AddressBook DBus callbacks
+     */
+    async _onObjectsAdded(connection, sender, path, iface, signal, params) {
+        try {
+            let adds = params.get_child_value(0).get_strv();
+
+            // NOTE: sequential pairs of vcard, id
+            for (let i = 0, len = adds.length; i < len; i += 2) {
+                try {
+                    let vcard = adds[i];
+                    let econtact = EBookContacts.Contact.new_from_vcard(vcard);
+                    let contact = await this._parseEContact(econtact);
+
+                    if (contact !== undefined) {
+                        this.add(contact, false);
+                    }
+                } catch (e) {
+                    debug(e);
+                }
+            }
+        } catch (e) {
+            debug(e);
+        }
+    }
+
+    _onObjectsRemoved(connection, sender, path, iface, signal, params) {
+        try {
+            let changes = params.get_child_value(0).get_strv();
+
+            for (let id of changes) {
+                try {
+                    this.remove(id, false);
+                } catch (e) {
+                    debug(e);
+                }
+            }
+        } catch (e) {
+            debug(e);
+        }
+    }
+
+    async _onObjectsModified(connection, sender, path, iface, signal, params) {
+        try {
+            let changes = params.get_child_value(0).get_strv();
+
+            // NOTE: sequential pairs of vcard, id
+            for (let i = 0, len = changes.length; i < len; i += 2) {
+                try {
+                    let vcard = changes[i];
+                    let econtact = EBookContacts.Contact.new_from_vcard(vcard);
+                    let contact = await this._parseEContact(econtact);
+
+                    if (contact !== undefined) {
+                        this.add(contact, false);
+                    }
+                } catch (e) {
+                    debug(e);
+                }
+            }
+        } catch (e) {
+            debug(e);
+        }
+    }
+
+    /*
+     * SourceRegistryWatcher callbacks
+     */
+    async _onAppeared(watcher, source) {
+        try {
+            // Get an EBookClient and EBookView
+            let uid = source.get_uid();
+            let client = await this._getEBookClient(source);
+            let view = await this._getEBookView(client);
+
+            // Watch the view for changes to the address book
+            let connection = view.get_connection();
+            let objectPath = view.get_object_path();
+
+            view._objectsAddedId = connection.signal_subscribe(
+                null,
+                'org.gnome.evolution.dataserver.AddressBookView',
+                'ObjectsAdded',
+                objectPath,
+                null,
+                Gio.DBusSignalFlags.NONE,
+                this._onObjectsAdded.bind(this)
+            );
+
+            view._objectsRemovedId = connection.signal_subscribe(
+                null,
+                'org.gnome.evolution.dataserver.AddressBookView',
+                'ObjectsRemoved',
+                objectPath,
+                null,
+                Gio.DBusSignalFlags.NONE,
+                this._onObjectsRemoved.bind(this)
+            );
+
+            view._objectsModifiedId = connection.signal_subscribe(
+                null,
+                'org.gnome.evolution.dataserver.AddressBookView',
+                'ObjectsModified',
+                objectPath,
+                null,
+                Gio.DBusSignalFlags.NONE,
+                this._onObjectsModified.bind(this)
+            );
+
+            view.start();
+
+            // Store the EBook in a map
+            this._ebooks.set(uid, {
+                source: source,
+                client: client,
+                view: view
+            });
+        } catch (e) {
+            debug(e);
+        }
+    }
+
+    _onDisappeared(watcher, source) {
+        try {
+            let uid = source.get_uid();
+            let ebook = this._ebooks.get(uid);
+
+            if (ebook === undefined)
+                return;
+
+            // Disconnect and dispose the EBookView
+            if (ebook.view) {
+                let connection = ebook.view.get_connection();
+                connection.signal_unsubscribe(ebook.view._objectsAddedId);
+                connection.signal_unsubscribe(ebook.view._objectsRemovedId);
+                connection.signal_unsubscribe(ebook.view._objectsModifiedId);
+                ebook.view.stop();
+
+                ebook.view.run_dispose();
+                ebook.view = null;
+            }
+
+            // Dispose the EBookClient
+            if (ebook.client) {
+                ebook.client.run_dispose();
+                ebook.client = null;
+            }
+
+            // Drop the EBook
+            this._ebooks.delete(uid);
+        } catch (e) {
+            debug(e);
+        }
+    }
+
+    async _initEvolutionDataServer() {
+        try {
+            // Get the current EBooks
+            let registry = await this._getESourceRegistry();
+
+            for (let source of registry.list_sources('Address Book')) {
+                await this._onAppeared(null, source);
+            }
+
+            // Watch for new and removed sources
+            this._watcher = new EDataServer.SourceRegistryWatcher({
+                registry: registry,
+                extension_name: 'Address Book'
+            });
+
+            this._appearedId = this._watcher.connect(
+                'appeared',
+                this._onAppeared.bind(this)
+            );
+            this._disappearedId = this._watcher.connect(
+                'disappeared',
+                this._onDisappeared.bind(this)
+            );
+        } catch (e) {
+            logError(e);
         }
     }
 
@@ -73,12 +382,6 @@ var Store = GObject.registerClass({
         this._context = context;
 
         if (context === null) {
-            // Create a re-usable launcher for folks.py
-            this._launcher = new Gio.SubprocessLauncher({
-                flags: Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
-            });
-            this._launcher.setenv('FOLKS_BACKENDS_DISABLED', 'telepathy', true);
-
             this.__cache_dir = Gio.File.new_for_path(gsconnect.cachedir);
         } else {
             this.__cache_dir = Gio.File.new_for_path(
@@ -317,35 +620,18 @@ var Store = GObject.registerClass({
         }
     }
 
+    /**
+     * Fetch and update the contact store from its source. The default function
+     * simply logs a debug message if EDS is unavailable, while derived classes
+     * should request an update from the remote source.
+     */
     async fetch() {
         try {
-            let folks = await new Promise((resolve, reject) => {
-                let proc = this._launcher.spawnv([
-                    gsconnect.extdatadir + '/service/components/folks.py'
-                ]);
-
-                proc.communicate_utf8_async(null, null, (proc, res) => {
-                    try {
-                        let [, stdout, stderr] = proc.communicate_utf8_finish(res);
-
-                        if (stderr.length > 0) {
-                            throw new Error(stderr);
-                        }
-
-                        resolve(JSON.parse(stdout));
-                    } catch (e) {
-                        // format python errors
-                        e.stack = e.message;
-                        e.message = e.stack.split('\n').filter(l => l).pop();
-
-                        reject(e);
-                    }
-                });
-            });
-
-            await this.update(folks);
+            if (HAVE_EDS === false) {
+                throw new Error('Evolution Data Server not available');
+            }
         } catch (e) {
-            debug(e, 'Loading folks');
+            debug(e);
         }
     }
 
@@ -366,6 +652,10 @@ var Store = GObject.registerClass({
      * Save the contacts to disk.
      */
     async save() {
+        if (this.context === null && HAVE_EDS) {
+            return;
+        }
+
         if (this.__cache_lock) {
             this.__cache_queue = true;
             return;
@@ -387,6 +677,21 @@ var Store = GObject.registerClass({
     }
 
     destroy() {
+        if (this.__disposed === undefined) {
+            this.__disposed = true;
+
+            if (HAVE_EDS) {
+                this._watcher.disconnect(this._appearedId);
+                this._watcher.disconnect(this._disappearedId);
+                this._watcher.run_dispose();
+
+                for (let ebook of this._ebooks.values()) {
+                    this._onDisappeared(null, ebook.source);
+                }
+            }
+
+            this.run_dispose();
+        }
     }
 });
 
