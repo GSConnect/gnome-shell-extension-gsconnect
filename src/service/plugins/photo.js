7bb80017553eb470a3a091445377dd8ce6606545
Andy Holmes <andrew.g.r.holmes@gmail.com>
2020-07-25 00:49:28 -0700

    Photo Plugin: cleanup, add failure notification

--- src/service/plugins/photo.js
+++ src/service/plugins/photo.js
@@ -10,8 +10,14 @@ const PluginsBase = imports.service.plugins.base;
 var Metadata = {
     label: _('Photo'),
     id: 'org.gnome.Shell.Extensions.GSConnect.Plugin.Photo',
-    incomingCapabilities: ['kdeconnect.photo', 'kdeconnect.photo.request'],
-    outgoingCapabilities: ['kdeconnect.photo', 'kdeconnect.photo.request'],
+    incomingCapabilities: [
+        'kdeconnect.photo',
+        'kdeconnect.photo.request'
+    ],
+    outgoingCapabilities: [
+        'kdeconnect.photo',
+        'kdeconnect.photo.request'
+    ],
     actions: {
         photo: {
             label: _('Photo'),
@@ -46,69 +52,83 @@ var Plugin = GObject.registerClass({
         });
     }
 
-    get camera() {
-        return this.settings.get_boolean('share-camera');
-    }
-
     handlePacket(packet) {
-        if (packet.type === 'kdeconnect.photo.request' && this.camera) {
-            this._sendPhoto();
-        } else if (packet.type === 'kdeconnect.photo') {
-            this._receivePhoto(packet);
+        switch (packet.type) {
+            case 'kdeconnect.photo':
+                this._receivePhoto(packet);
+                break;
+
+            case 'kdeconnect.photo.request':
+                this._sendPhoto(packet);
+                break;
         }
     }
 
+    /**
+     * Ensure we have a directory set for storing files that exists.
+     */
     _ensureReceiveDirectory() {
-        let receiveDir = this.settings.get_string('receive-directory');
+        if (this._receiveDir !== undefined)
+            return this._receiveDir;
 
         // Ensure a directory is set
-        if (!receiveDir) {
-            receiveDir = GLib.get_user_special_dir(
+        this._receiveDir = this.settings.get_string('receive-directory');
+
+        if (this._receiveDir === '') {
+            this._receiveDir = GLib.get_user_special_dir(
                 GLib.UserDirectory.DIRECTORY_PICTURES
             );
 
             // Fallback to ~/Pictures
             let homeDir = GLib.get_home_dir();
 
-            if (!receiveDir || receiveDir === homeDir) {
-                receiveDir = GLib.build_filenamev([homeDir, 'Pictures']);
+            if (!this._receiveDir || this._receiveDir === homeDir) {
+                this._receiveDir = GLib.build_filenamev([homeDir, 'Pictures']);
+                this.settings.set_string('receive-directory', this._receiveDir);
             }
-
-            this.settings.set_string('receive-directory', receiveDir);
         }
 
         // Ensure the directory exists
-        if (!GLib.file_test(receiveDir, GLib.FileTest.IS_DIR)) {
-            GLib.mkdir_with_parents(receiveDir, 448);
-        }
+        if (!GLib.file_test(this._receiveDir, GLib.FileTest.IS_DIR))
+            GLib.mkdir_with_parents(this._receiveDir, 448);
 
-        return receiveDir;
+        return this._receiveDir;
     }
 
+    /**
+     * Get a GFile for @filename, while ensuring the directory exists and the
+     * file is unique.
+     *
+     * @param {string} filename - A filename (eg. `image.jpg`)
+     * @return {Gio.File} a file object
+     */
     _getFile(filename) {
         let dirpath = this._ensureReceiveDirectory();
         let basepath = GLib.build_filenamev([dirpath, filename]);
         let filepath = basepath;
         let copyNum = 0;
 
-        while (GLib.file_test(filepath, GLib.FileTest.EXISTS)) {
-            copyNum += 1;
-            filepath = `${basepath} (${copyNum})`;
-        }
+        while (GLib.file_test(filepath, GLib.FileTest.EXISTS))
+            filepath = `${basepath} (${++copyNum})`;
 
         return Gio.File.new_for_path(filepath);
     }
 
+    /**
+     * Receive a photo taken by the remote device.
+     *
+     * @param {Core.Packet} packet - a `kdeconnect.photo`
+     */
     async _receivePhoto(packet) {
-        let file, stream, success, transfer;
-
         try {
             // Remote device cancelled the photo operation
-            if (packet.body.cancel) return;
+            if (packet.body.hasOwnProperty('cancel'))
+                return;
 
-            file = this._getFile(packet.body.filename);
+            // Open the target path and create a transfer
+            let file = this._getFile(packet.body.filename);
 
-            stream = await new Promise((resolve, reject) => {
+            let stream = await new Promise((resolve, reject) => {
                 file.replace_async(null, false, 0, 0, null, (file, res) => {
                     try {
                         resolve(file.replace_finish(res));
@@ -118,32 +138,33 @@ var Plugin = GObject.registerClass({
                 });
             });
 
-            transfer = this.device.createTransfer(Object.assign({
+            let transfer = this.device.createTransfer(Object.assign({
                 output_stream: stream,
                 size: packet.payloadSize
             }, packet.payloadTransferInfo));
 
-            // Start transfer
-            success = await transfer.download(packet.payloadTransferInfo.port);
+            // Open the photo if successful, delete on failure
+            let success = await transfer.download();
 
-            // Open the photo on success
-            if (success) {
-                let uri = file.get_uri();
-                Gio.AppInfo.launch_default_for_uri_async(uri, null, null, null);
-
-            // Clean up the downloaded file on failure
-            } else {
-                file.delete(null);
+            if (!success) {
+                file.delete_async(GLib.PRIORITY_DEFAULT, null, null);
+                return;
             }
+
+            let uri = file.get_uri();
+            Gio.AppInfo.launch_default_for_uri_async(uri, null, null, null);
         } catch (e) {
-            logError(e);
+            debug(e, this.device.name);
         }
     }
 
     /**
-     * Take a photo using the Webcam, return a file path
+     * Take a photo using the Webcam and return the path.
+     *
+     * @param {Core.Packet} packet - A `kdeconnect.photo.request`
+     * @return {Promise<string>} A file path
      */
-    _takePhoto() {
+    _takePhoto(packet) {
         return new Promise((resolve, reject) => {
             let time = GLib.DateTime.new_now_local().format('%T');
             let path = GLib.build_filenamev([GLib.get_tmp_dir(), `${time}.jpg`]);
@@ -167,19 +188,26 @@ var Plugin = GObject.registerClass({
         });
     }
 
-    async _sendPhoto() {
-        let file, path, stream, transfer;
+    /**
+     * Send a photo to the remote device.
+     *
+     * @param {Core.Packet} packet - A `kdeconnect.photo.request`
+     */
+    async _sendPhoto(packet) {
+        if (this.settings.get_boolean('share-camera'))
+            return;
 
         try {
-            path = await this._takePhoto();
+            let file = null;
+            let path = await this._takePhoto();
 
-            if (path.startsWith('file://')) {
+            if (path.startsWith('file://'))
                 file = Gio.File.new_for_uri(path);
-            } else {
+            else
                 file = Gio.File.new_for_path(path);
-            }
 
-            stream = await new Promise((resolve, reject) => {
+            // Prepare the file for upload
+            let stream = new Promise((resolve, reject) => {
                 file.read_async(GLib.PRIORITY_DEFAULT, null, (file, res) => {
                     try {
                         resolve(file.read_finish(res));
@@ -189,22 +217,57 @@ var Plugin = GObject.registerClass({
                 });
             });
 
-            transfer = this.device.createTransfer({
-                input_stream: stream,
-                size: file.query_info('standard::size', 0, null).get_size()
+            let info = new Promise((resolve, reject) => {
+                file.query_info_async(
+                    'standard::size',
+                    Gio.FileQueryInfoFlags.NONE,
+                    GLib.PRIORITY_DEFAULT,
+                    null,
+                    (file, res) => {
+                        try {
+                            resolve(file.query_info_finish(res));
+                        } catch (e) {
+                            reject(e);
+                        }
+                    }
+                );
             });
 
-            await transfer.upload({
+            await Promise.all([stream, info]);
+
+            // Transfer
+            let transfer = this.device.createTransfer({
+                input_stream: stream,
+                size: info.get_size()
+            });
+
+            let success = await transfer.upload({
                 type: 'kdeconnect.photo',
                 body: {
                     filename: file.get_basename()
                 }
             });
+
+            if (!success) {
+                this.device.showNotification({
+                    id: transfer.uuid,
+                    title: _('Transfer Failed'),
+                    // TRANSLATORS: eg. Failed to send "photo.jpg" to Google Pixel
+                    body: _('Failed to send “%s” to %s').format(
+                        file.get_basename(),
+                        this.device.name
+                    ),
+                    icon: new Gio.ThemedIcon({name: 'dialog-warning-symbolic'})
+                });
+            }
         } catch (e) {
             debug(e, this.device.name);
         }
     }
 
+    /**
+     * Request the remote device begin a photo operation.
+     */
     photo() {
         this.device.sendPacket({
             type: 'kdeconnect.photo.request',
