2ca2910256f085ac8303cdd23e54093745144457
Andy Holmes <andrew.g.r.holmes@gmail.com>
2020-08-10 18:53:09 -0700

    meson: cleanup mkzip.sh

--- meson/mkzip.sh
+++ meson/mkzip.sh
@@ -1,38 +1,30 @@
 #!/bin/sh
 
-ZIP_DESTDIR="${MESON_BUILD_ROOT}/_zip"
+export DESTDIR="${MESON_BUILD_ROOT}/_zip"
+
 ZIP_DIR="${MESON_BUILD_ROOT}/${UUID}"
 ZIP_FILE="${MESON_BUILD_ROOT}/${UUID}.zip"
 
-GSCHEMA_DIR="${ZIP_DESTDIR}/${GSCHEMADIR}"
-LOCALE_DIR="${ZIP_DESTDIR}/${LOCALEDIR}"
 
 # PRE-CLEAN
-rm -rf ${ZIP_DESTDIR} ${ZIP_DIR} ${ZIP_FILE}
+rm -rf ${DESTDIR} ${ZIP_DIR} ${ZIP_FILE}
 
 # BUILD
-cd ${MESON_BUILD_ROOT}
-DESTDIR=${ZIP_DESTDIR} ninja install
+if ! ninja -C ${MESON_BUILD_ROOT} install > /dev/null; then
+  exit 1;
+fi
 
 # COPY
 mkdir -p ${ZIP_DIR}
-cp -pr ${ZIP_DESTDIR}/${DATADIR}/gnome-shell/extensions/${UUID}/* ${ZIP_DIR}
-
-cp -pr ${GSCHEMA_DIR} ${ZIP_DIR}
-cp -pr ${ZIP_DESTDIR}/${DATADIR}/nautilus-python/extensions/* ${ZIP_DIR}
+cp -pr ${DESTDIR}/${DATADIR}/gnome-shell/extensions/${UUID}/* ${ZIP_DIR}
+cp -pr ${DESTDIR}/${DATADIR}/nautilus-python/extensions/* ${ZIP_DIR}
+cp -pr ${DESTDIR}/${LOCALEDIR} ${ZIP_DIR}
+cp -pr ${DESTDIR}/${GSCHEMADIR} ${ZIP_DIR}
 glib-compile-schemas ${ZIP_DIR}/schemas
-
-if [ -d ${LOCALE_DIR} ]; then
-    cp -pr ${LOCALE_DIR} ${ZIP_DIR}
-fi
 
 # COMPRESS
 cd ${ZIP_DIR}
 zip -qr ${ZIP_FILE} .
-cd ${MESON_BUILD_ROOT}
-rm -rf _zip
-
-echo
 echo "Extension saved to ${ZIP_FILE}"
 
 # INSTALL
@@ -42,8 +34,8 @@ if [ "$INSTALL" = true ]; then
 
     mkdir -p ${EXTENSIONS_DIR}
     rm -rf ${INSTALL_DIR}
-    unzip ${ZIP_FILE} -d ${INSTALL_DIR}
+    unzip -q ${ZIP_FILE} -d ${INSTALL_DIR}
 
-    echo "Extension installed to $INSTALL_DIR"
+    echo "Extension installed to ${INSTALL_DIR}"
 fi
 
