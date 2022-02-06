be8109995f66bb209315703ff11c44f13b672514
everyx <lunt.luo@gmail.com>
2022-02-06 22:13:52 +0800

    fix: prefer text/plain;charset=utf-8
    Close: #1244

--- src/shell/clipboard.js
+++ src/shell/clipboard.js
@@ -209,7 +209,7 @@ var Clipboard = GObject.registerClass({
             const mimetypes = this._selection.get_mimetypes(
                 Meta.SelectionType.SELECTION_CLIPBOARD);
 
-            const mimetype = mimetypes.find(type => TEXT_MIMETYPES.includes(type));
+            const mimetype = TEXT_MIMETYPES.find(type => mimetypes.includes(type));
 
             if (mimetype !== undefined) {
                 const stream = Gio.MemoryOutputStream.new_resizable();
