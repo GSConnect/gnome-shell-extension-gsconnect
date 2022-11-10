96b4b2ded98edca632809d674fae10c4ff1c5b86
Nath <nathanlloyd@protonmail.com>
2022-11-10 22:48:39 +0000

    Update nautilus-gsconnect.py
    Just made it shorter and more readable.  but found out that this script hasn't got any access to the notification system to provide a progress bar.  how would one modify `send_files` to allow how complete is the process.  I have an habit to transmit >100MB files between my computer and phone which takes a few minutes it would be nice to check my notification panel and see how far it is to finishing and maybe an ETA that is updated realtime

--- nautilus-extension/nautilus-gsconnect.py
+++ nautilus-extension/nautilus-gsconnect.py
@@ -2,85 +2,75 @@
 nautilus-gsconnect.py - A Nautilus extension for sending files via GSConnect.
 
 A great deal of credit and appreciation is owed to the indicator-kdeconnect
-developers for the sister Python script 'kdeconnect-send-nautilus.py':
+developers for the sister Python script "kdeconnect-send-nautilus.py":
 
 https://github.com/Bajoja/indicator-kdeconnect/blob/master/data/extensions/kdeconnect-send-nautilus.py
 """
 
-import gettext
-import os.path
-import sys
+from gettext import translation
+from os.path import join, exists
+from sys import argv
 
 import gi
-gi.require_version('Gio', '2.0')
-gi.require_version('GLib', '2.0')
-gi.require_version('GObject', '2.0')
-from gi.repository import Gio, GLib, GObject
+gi.require_version("Gio", "2.0")
+gi.require_version("GLib", "2.0")
+gi.require_version("GObject", "2.0")
+from gi.repository.Gio import (
+    DBusProxy, DBusProxyFlags, BusType, DBusActionGroup, DBusCallFlags)
+from gi.repository.GLib import get_user_data_dir, Variant
+from gi.repository.GObject import Object
 
 
 # Host application detection
 #
-# Nemo seems to reliably identify itself as 'nemo' in argv[0], so we
+# Nemo seems to reliably identify itself as "nemo" in argv[0], so we
 # can test for that. Nautilus detection is less reliable, so don't try.
 # See https://github.com/linuxmint/nemo-extensions/issues/330
-if "nemo" in sys.argv[0].lower():
+if "nemo" in argv[0].lower():
     # Host runtime is nemo-python
-    gi.require_version('Nemo', '3.0')
+    gi.require_version("Nemo", "3.0")
     from gi.repository import Nemo as FileManager
 else:
     # Otherwise, just assume it's nautilus-python
     from gi.repository import Nautilus as FileManager
 
 
-SERVICE_NAME = 'org.gnome.Shell.Extensions.GSConnect'
-SERVICE_PATH = '/org/gnome/Shell/Extensions/GSConnect'
+SERVICE_NAME = "org.gnome.Shell.Extensions.GSConnect"
+SERVICE_PATH = "/org/gnome/Shell/Extensions/GSConnect"
 
 # Init gettext translations
-LOCALE_DIR = os.path.join(GLib.get_user_data_dir(),
-                          'gnome-shell', 'extensions',
-                          'gsconnect@andyholmes.github.io', 'locale')
+LOCALE_DIR = join(
+    get_user_data_dir(), "gnome-shell", "extensions",
+    "gsconnect@andyholmes.github.io", "locale")
 
-if not os.path.exists(LOCALE_DIR):
+if not exists(LOCALE_DIR):
     LOCALE_DIR = None
 
 try:
-    i18n = gettext.translation(SERVICE_NAME,
-                               localedir=LOCALE_DIR)
+    i18n = translation(SERVICE_NAME, localedir=LOCALE_DIR)
     _ = i18n.gettext
-
-except (IOError, OSError) as e:
-    print('GSConnect: {0}'.format(e.strerror))
-    i18n = gettext.translation(SERVICE_NAME,
-                               localedir=LOCALE_DIR,
-                               fallback=True)
+except (IOError, OSError) as error:
+    print(f"GSConnect: {error.strerror}")
+    i18n = translation(SERVICE_NAME, localedir=LOCALE_DIR, fallback=True)
     _ = i18n.gettext
 
 
-class GSConnectShareExtension(GObject.Object, FileManager.MenuProvider):
+class GSConnectShareExtension(Object, FileManager.MenuProvider):
     """A context menu for sending files via GSConnect."""
 
     def __init__(self):
         """Initialize the DBus ObjectManager"""
-
-        GObject.Object.__init__(self)
-
-        self.devices = {}
-
-        Gio.DBusProxy.new_for_bus(Gio.BusType.SESSION,
-                                  Gio.DBusProxyFlags.DO_NOT_AUTO_START,
-                                  None,
-                                  SERVICE_NAME,
-                                  SERVICE_PATH,
-                                  'org.freedesktop.DBus.ObjectManager',
-                                  None,
-                                  self._init_async,
-                                  None)
+        Object.__init__(self)
+        self.devices = dict()
+        DBusProxy.new_for_bus(
+            BusType.SESSION, DBusProxyFlags.DO_NOT_AUTO_START, None,
+            SERVICE_NAME, SERVICE_PATH, "org.freedesktop.DBus.ObjectManager",
+            None, self._init_async, None)
 
     def _init_async(self, proxy, res, user_data):
         proxy = proxy.new_for_bus_finish(res)
-        proxy.connect('notify::g-name-owner', self._on_name_owner_changed)
-        proxy.connect('g-signal', self._on_g_signal)
-
+        proxy.connect("notify::g-name-owner", self._on_name_owner_changed)
+        proxy.connect("g-signal", self._on_g_signal)
         self._on_name_owner_changed(proxy, None)
 
     def _on_g_signal(self, proxy, sender_name, signal_name, parameters):
@@ -90,16 +80,13 @@ class GSConnectShareExtension(GObject.Object, FileManager.MenuProvider):
 
         objects = parameters.unpack()
 
-        if signal_name == 'InterfacesAdded':
+        if signal_name == "InterfacesAdded":
             for object_path, props in objects.items():
-                props = props['org.gnome.Shell.Extensions.GSConnect.Device']
-
-                self.devices[object_path] = (props['Name'],
-                                             Gio.DBusActionGroup.get(
-                                                 proxy.get_connection(),
-                                                 SERVICE_NAME,
-                                                 object_path))
-        elif signal_name == 'InterfacesRemoved':
+                props = props["org.gnome.Shell.Extensions.GSConnect.Device"]
+                self.devices[object_path] = (
+                    props["Name"], DBusActionGroup.get(
+                        proxy.get_connection(), SERVICE_NAME, object_path))
+        elif signal_name == "InterfacesRemoved":
             for object_path in objects:
                 try:
                     del self.devices[object_path]
@@ -109,36 +96,29 @@ class GSConnectShareExtension(GObject.Object, FileManager.MenuProvider):
     def _on_name_owner_changed(self, proxy, pspec):
         # Wait until the service is ready
         if proxy.props.g_name_owner is None:
-            self.devices = {}
+            self.devices = dict()
         else:
-            proxy.call('GetManagedObjects',
-                       None,
-                       Gio.DBusCallFlags.NO_AUTO_START,
-                       -1,
-                       None,
-                       self._get_managed_objects,
-                       None)
+            proxy.call(
+                "GetManagedObjects", None, DBusCallFlags.NO_AUTO_START, -1,
+                None, self._get_managed_objects, None)
 
     def _get_managed_objects(self, proxy, res, user_data):
         objects = proxy.call_finish(res)[0]
 
         for object_path, props in objects.items():
-            props = props['org.gnome.Shell.Extensions.GSConnect.Device']
+            props = props["org.gnome.Shell.Extensions.GSConnect.Device"]
             if not props:
                 continue
-
-            self.devices[object_path] = (props['Name'],
-                                         Gio.DBusActionGroup.get(
-                                             proxy.get_connection(),
-                                             SERVICE_NAME,
-                                             object_path))
+            self.devices[object_path] = (
+                props["Name"], DBusActionGroup.get(
+                    proxy.get_connection(), SERVICE_NAME, object_path))
 
     def send_files(self, menu, files, action_group):
         """Send *files* to *device_id*"""
 
         for file in files:
-            variant = GLib.Variant('(sb)', (file.get_uri(), False))
-            action_group.activate_action('shareFile', variant)
+            variant = Variant("(sb)", (file.get_uri(), False))
+            action_group.activate_action("shareFile", variant)
 
     def get_file_items(self, *args):
         # `args` will be `[files: List[Nautilus.FileInfo]]` in Nautilus 4.0 API,
@@ -148,25 +128,24 @@ class GSConnectShareExtension(GObject.Object, FileManager.MenuProvider):
 
         # Only accept regular files
         for uri in files:
-            if uri.get_uri_scheme() != 'file' or uri.is_directory():
-                return ()
+            if uri.get_uri_scheme() != "file" or uri.is_directory():
+                return tuple()
 
         # Enumerate capable devices
-        devices = []
+        devices = list()
 
         for name, action_group in self.devices.values():
-            if action_group.get_action_enabled('shareFile'):
+            if action_group.get_action_enabled("shareFile"):
                 devices.append([name, action_group])
 
         # No capable devices; don't show menu entry
         if not devices:
-            return ()
+            return tuple()
 
         # Context Menu Item
         menu = FileManager.MenuItem(
-            name='GSConnectShareExtension::Devices',
-            label=_('Send To Mobile Device')
-        )
+            name="GSConnectShareExtension::Devices",
+            label=_("Send To Mobile Device"))
 
         # Context Submenu
         submenu = FileManager.Menu()
@@ -175,13 +154,8 @@ class GSConnectShareExtension(GObject.Object, FileManager.MenuProvider):
         # Context Submenu Items
         for name, action_group in devices:
             item = FileManager.MenuItem(
-                name='GSConnectShareExtension::Device' + name,
-                label=name
-            )
-
-            item.connect('activate', self.send_files, files, action_group)
-
+                name="GSConnectShareExtension::Device" + name, label=name)
+            item.connect("activate", self.send_files, files, action_group)
             submenu.append_item(item)
 
         return (menu,)
-
