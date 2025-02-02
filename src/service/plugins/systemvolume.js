59d09bde2f08974843a635ed24992881a9397cab
FeRD (Frank Dana) <ferdnyc@gmail.com>
2025-02-02 14:46:41 -0500

    Load Gvc in systemvolume plugin

--- src/service/plugins/systemvolume.js
+++ src/service/plugins/systemvolume.js
@@ -2,13 +2,25 @@
 //
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+import GIRepository from 'gi://GIRepository';
+import GLib from 'gi://GLib';
 import GObject from 'gi://GObject';
 
 import * as Components from '../components/index.js';
 import Config from '../../config.js';
 import * as Core from '../core.js';
 import Plugin from '../plugin.js';
-import {Gvc} from '../components/pulseaudio.js';
+
+
+let Gvc = null;
+try {
+    // Add gnome-shell's typelib dir to the search path
+    const typelibDir = GLib.build_filenamev([Config.GNOME_SHELL_LIBDIR, 'gnome-shell']);
+    GIRepository.Repository.prepend_search_path(typelibDir);
+    GIRepository.Repository.prepend_library_path(typelibDir);
+
+    Gvc = (await import('gi://Gvc')).default;
+} catch {}
 
 
 export const Metadata = {
