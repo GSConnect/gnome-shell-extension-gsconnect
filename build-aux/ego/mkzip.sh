d91aa1b9f32c8428ec8565feb58bfeff75030a9e
FeRD (Frank Dana) <ferdnyc@gmail.com>
2025-02-26 02:37:27 -0500

    Since GNOME 44, we shouldn't compile our schema
    The extension system will compile it when installing the extension.
    
    See: https://gjs.guide/extensions/upgrading/gnome-shell-44.html#gsettings-schema

--- build-aux/ego/mkzip.sh
+++ build-aux/ego/mkzip.sh
@@ -24,7 +24,6 @@ cp -pr ${DESTDIR}/${DATADIR}/gnome-shell/extensions/${UUID}/* ${ZIP_DIR}
 cp -pr ${DESTDIR}/${DATADIR}/nautilus-python/extensions/* ${ZIP_DIR}
 cp -pr ${DESTDIR}/${LOCALEDIR} ${ZIP_DIR}
 cp -pr ${DESTDIR}/${GSCHEMADIR} ${ZIP_DIR}
-glib-compile-schemas ${ZIP_DIR}/schemas
 
 # Stop without zipping dir, if requested
 if [ "$NOZIP" = true ]; then
