e156f498767ce3d3493483f095fef9f6dd83beb4
Andy Holmes <andrew.g.r.holmes@gmail.com>
2025-03-19 20:44:43 -0700

    fix(lan): fix the deviceId and deviceName checks
    The device ID and name checks were being performed on the local
    identity packet, instead of the incoming packet.

--- src/service/backends/lan.js
+++ src/service/backends/lan.js
@@ -361,15 +361,15 @@ export const ChannelService = GObject.registerClass({
                 return;
 
             // Reject invalid device IDs
-            if (!Device.validateId(this.identity.body.deviceId))
-                throw new Error(`invalid deviceId "${this.identity.body.deviceId}"`);
+            if (!Device.validateId(packet.body.deviceId))
+                throw new Error(`invalid deviceId "${packet.body.deviceId}"`);
 
-            if (!this.identity.body.deviceName)
+            if (!packet.body.deviceName)
                 throw new Error('missing deviceName');
 
             // Reject invalid device names
-            if (!Device.validateName(this.identity.body.deviceName))
-                throw new Error(`invalid deviceName "${this.identity.body.deviceName}"`);
+            if (!Device.validateName(packet.body.deviceName))
+                throw new Error(`invalid deviceName "${packet.body.deviceName}"`);
 
             debug(packet);
 
