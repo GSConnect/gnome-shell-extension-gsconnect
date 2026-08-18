83b9c176e560dc5aa08a68d97b10f429c60f2b4b
DaPigGuy <mcpepig123@gmail.com>
2026-08-17 23:15:32 -0700

    Add helper to wrap object to create DBus interface

--- src/service/utils/dbus.js
+++ src/service/utils/dbus.js
@@ -258,6 +258,79 @@ export const Interface = GObject.registerClass({
 });
 
 /**
+ * Creates a DBus interface which performs property get/set and method
+ * calls on the an GObject, using the `Gio.DBusExportedObject.wrapJSObject`
+ * convenience helper from GJS.
+ *
+ * On top of the convenience helper, handle translating DBus-style property
+ * names to JS-style names, emitting property changed signals to DBus, and
+ * emitting DBus interface signals.
+ *
+ * @param {Gio.DBusInterfaceInfo} interfaceInfo - The info for a DBus interface
+ * @param {GObject.Object} obj - The object with DBus properties and methods
+ * @returns {Gio.DBusExportedObject} - The DBus interface
+ */
+export function wrapObject(interfaceInfo, obj) {
+    // Intercept property get/set on the object to translate property names to
+    // JS-style names if necessary.
+    const proxyObj = new Proxy(
+        obj,
+        {
+            get(target, prop, receiver) {
+                const value = Reflect.has(target, prop)
+                    ? Reflect.get(target, prop)
+                    : Reflect.get(target, toUnderscoreCase(prop));
+
+                // `this` should not be pointing at Proxy
+                if (typeof value === 'function')
+                    return value.bind(target);
+
+                return value;
+            },
+            set(target, prop, newValue, receiver) {
+                return Reflect.has(target, prop)
+                    ? Reflect.set(target, prop, newValue)
+                    : Reflect.set(target, toUnderscoreCase(prop), newValue);
+            },
+        }
+    );
+    const impl = Gio.DBusExportedObject.wrapJSObject(interfaceInfo, proxyObj);
+
+    // Automatically forward known signals
+    obj.connect(
+        'notify',
+        (_, pspec) => {
+            const name = toDBusCase(pspec.name);
+            const propertyInfo = interfaceInfo.lookup_property(name);
+
+            if (propertyInfo === null)
+                return;
+
+            impl.emit_property_changed(
+                name,
+                new GLib.Variant(
+                    propertyInfo.signature,
+                    // Adjust for GJS's '-'/'_' conversion
+                    obj[pspec.name.replace(/-/gi, '_')]
+                )
+            );
+        }
+    );
+
+    for (const signal of interfaceInfo.signals) {
+        const type = `(${signal.args.map(arg => arg.signature).join('')})`;
+        obj.connect(
+            signal.name,
+            (_, ...args) => {
+                impl.emit_signal(signal.name, new GLib.Variant(type, args));
+            }
+        );
+    }
+
+    return impl;
+}
+
+/**
  * Get a new, dedicated DBus connection on {@link busType}
  *
  * @param {Gio.BusType} [busType] - a Gio.BusType constant
